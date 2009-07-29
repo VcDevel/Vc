6e0a63ddfe337539567f7661407cd520d69c65d0
Matthias Kretz <Matthias.Kretz@kip.uni-heidelberg.de>
2009-07-29 10:44:03 +0200

    switch alpha and x to get the best of madd132

--- benchmarks/flops.cpp
+++ benchmarks/flops.cpp
@@ -121,10 +121,10 @@ int main()
             ///////////////////////////////////////
 
             for (int i = 0; i < factor; ++i) {
-                    x[0] = _mm512_madd132_ps(alpha[0], y[0], x[0]);
-                    x[1] = _mm512_madd132_ps(alpha[1], y[1], x[1]);
-                    x[2] = _mm512_madd132_ps(alpha[2], y[2], x[2]);
-                    x[3] = _mm512_madd132_ps(alpha[3], y[3], x[3]);
+                    x[0] = _mm512_madd132_ps(x[0], y[0], alpha[0]);
+                    x[1] = _mm512_madd132_ps(x[1], y[1], alpha[1]);
+                    x[2] = _mm512_madd132_ps(x[2], y[2], alpha[2]);
+                    x[3] = _mm512_madd132_ps(x[3], y[3], alpha[3]);
             }
 
             ///////////////////////////////////////
