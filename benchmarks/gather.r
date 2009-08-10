ea4fd64d70a19aba7055cba2994cedf1223e0568
Matthias Kretz <Matthias.Kretz@kip.uni-heidelberg.de>
2009-08-10 21:34:34 +0200

    cleanup; show L1, L2 and Cacheline sizes in the title

--- benchmarks/gather.r
+++ benchmarks/gather.r
@@ -1,10 +1,6 @@
 par(cex=0.75)
 #par(mfrow=c(2,1))
 
-colors  <- rainbow(5, v=0.3)
-lcolors <- rainbow(5, v=0.3, alpha=0.5)
-bgs     <- rainbow(5, v=0.8)
-
 gatherProcessData <- function(data) {
     data <- processData(data, paste(
             data$benchmark.name,
@@ -26,6 +22,17 @@ gatherProcessData <- function(data) {
 gatherChart <- function(data, orderfun, legendpos = "bottomright", column = "Values_per_cycle",
     xlab = "Values per Cycle", main = "Vector Gather")
 {
+    l2info <- NULL
+    l1info <- NULL
+    clinfo <- NULL
+    if(!is.null(data$L2.size)) l2info <- paste("L2:", as.character(data$L2.size[[1]]))
+    if(!is.null(data$L1.size)) l1info <- paste("L1:", as.character(data$L1.size[[1]]))
+    if(!is.null(data$Cacheline.size)) clinfo <- paste("Cacheline:", as.character(data$Cacheline.size[[1]]))
+    cacheinfo <- paste(c(l2info, l1info, clinfo), collapse=", ")
+    if(nchar(cacheinfo) > 0) {
+        main <- paste(main, " (", cacheinfo, ")", sep="")
+    }
+
     medianCol <- paste(column, ".median", sep="")
     meanCol   <- paste(column, ".mean"  , sep="")
     stddevCol <- paste(column, ".stddev", sep="")
@@ -45,6 +52,27 @@ gatherChart <- function(data, orderfun, legendpos = "bottomright", column = "Val
     }
     legend(legendpos, legendtext[21:23], pch=21:23)
     invisible(chart)
+}
+
+sse$Cacheline.size <- paste(as.character(sse$Cacheline.size), "B")
+lrb$Cacheline.size <- paste(as.character(lrb$Cacheline.size), "B")
+simple$Cacheline.size <- paste(as.character(simple$Cacheline.size), "B")
+
+sse$L1.size <- paste(as.character(sse$L1.size / 1024), "KiB")
+lrb$L1.size <- paste(as.character(lrb$L1.size / 1024), "KiB")
+simple$L1.size <- paste(as.character(simple$L1.size / 1024), "KiB")
+
+sse$L2.size <- sse$L2.size / 1024
+lrb$L2.size <- lrb$L2.size / 1024
+simple$L2.size <- simple$L2.size / 1024
+if(sse$L2.size[[1]] >= 1024) {
+    sse$L2.size <- paste(as.character(sse$L2.size / 1024), "MiB")
+    lrb$L2.size <- paste(as.character(lrb$L2.size / 1024), "MiB")
+    simple$L2.size <- paste(as.character(simple$L2.size / 1024), "MiB")
+} else {
+    sse$L2.size <- paste(as.character(sse$L2.size), "KiB")
+    lrb$L2.size <- paste(as.character(lrb$L2.size), "KiB")
+    simple$L2.size <- paste(as.character(simple$L2.size), "KiB")
 }
 
 for(data in list(rbind(sse, simple, lrb), rbind(sse, simple))) {
@@ -114,14 +142,5 @@ speedup <- rbind(
 gatherChart(speedup, function(d) { order(d$datatype, d$benchmark.name) }, column = "speedup", xlab =
 "Speedup", main = "Vector Gather: LRB Prototype vs. SSE")
 abline(v = 1, lty = "dashed", col = hsv(s = 1, v = 0, alpha = 0.4))
-
-
-#    data$fullkey  <- sub(" Masked", "", paste(data$benchmark.name, data$datatype, data$benchmark.arch, sep=", "))
-#    data$fullkey2 <- sub(" Masked", "", paste(data$datatype, data$benchmark.arch, data$benchmark.name, sep=", "))
-#    data$fullkey3 <- sub(" Masked", "", paste(data$benchmark.arch, data$datatype, data$benchmark.name, sep=", "))
-#        mychart2(Values_per_cycle, fullkey2, xlab="Values per Cycle", main=main[[1]], lcolor=lcolors, bg=bgs, color=colors, pch=pch)
-#        mychart2(Values_per_cycle, fullkey2, xlab="Values per Cycle", main=main[[1]], lcolor=lcolors, bg=bgs, color=colors, groups=sortkey(datatype, Values_per_cycle, fullkey2) * -1, pch=pch)
-#        mychart2(Values_per_cycle, fullkey3, xlab="Values per Cycle", main=main[[1]], lcolor=lcolors, bg=bgs, color=colors, groups=sortkey(benchmark.arch, Values_per_cycle, fullkey3) * -1, pch=pch)
-#        mychart2(Values_per_cycle, fullkey, xlab="Values per Cycle", main=main[[1]],  lcolor=rep(lcolors, each=n/5), bg=rep(bgs, each=n/5), color=rep(colors, each=n/5), groups=sortkey(benchmark.name, Values_per_cycle, fullkey) * -1, pch=pch)
 
 # vim: sw=4 et filetype=r sts=4 ai
