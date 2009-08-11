142fda56963652afb68956e127dd25235dd00a88
Matthias Kretz <Matthias.Kretz@kip.uni-heidelberg.de>
2009-08-11 15:49:24 +0200

    add possibility to plot with offsets, add speedup plot function

--- benchmarks/common.r
+++ benchmarks/common.r
@@ -11,7 +11,7 @@ mydotchart <- function(x, labels = NULL, groups = NULL, gdata = NULL, errors = N
     mean = NULL, cex = par("cex"),
     pch = 21, gpch = 21, bg = par("bg"), color = par("fg"), gcolor = par("fg"),
     lcolor = "gray", xlim = range(x[is.finite(x)]), main = NULL,
-    xlab = NULL, ylab = NULL, ...)
+    xlab = NULL, ylab = NULL, yoffset = 0, ...)
 {
     opar <- par("mai", "mar", "cex", "yaxs")
     on.exit(par(opar))
@@ -76,12 +76,12 @@ mydotchart <- function(x, labels = NULL, groups = NULL, gdata = NULL, errors = N
         mtext(labs, side = 2, line = loffset, at = y, adj = 0,
             col = color, las = 2, cex = cex, ...)
     }
-    abline(h = y, lty = "dotted", col = lcolor)
+    abline(h = y + yoffset, lty = "dotted", col = lcolor)
     if (!is.null(errors)) {
         if (is.null(mean)) mean <- x
-        errorbars(mean, y, xerr=errors, col = color)
+        errorbars(mean, y + yoffset, xerr=errors, col = color)
     }
-    points(x, y, pch = pch, col = color, bg = bg)
+    points(x, y + yoffset, pch = pch, col = color, bg = bg)
     if (!is.null(groups)) {
         gpos <- rev(cumsum(rev(tapply(groups, groups, length)) +
             2) - 1)
@@ -98,7 +98,7 @@ mydotchart <- function(x, labels = NULL, groups = NULL, gdata = NULL, errors = N
     axis(1)
     box()
     title(main = main, xlab = xlab, ylab = ylab, ...)
-    invisible(list(y=y, o=o, cex=cex, nmai=nmai))
+    invisible(list(y=y + yoffset, o=o, cex=cex, nmai=nmai))
 }
 
 addPoints <- function(chart, x, errors = NULL, mean = NULL,
@@ -170,7 +170,7 @@ mychart3 <- function(data, key, ncolors = NULL, xlim = NULL, ...) {
             tmp <- rgb2hsv(col2rgb(data$color))
             hsv(h=tmp[row(tmp) == 1], s=tmp[row(tmp) == 2], v=0.8 * tmp[row(tmp) == 3], alpha=0.5)
         }
-    if(is.null(data$pch    )) data$pch     <- c(21:31, 1:20)[as.integer(factor(data$benchmark.name))]
+    if(is.null(data$pch    )) data$pch     <- c(21:25, 3:20)[as.integer(factor(data$benchmark.name))]
 
     if(is.null(xlim)) xlim <- range(0, mean + stddev, median)
 
@@ -191,29 +191,44 @@ mychart3 <- function(data, key, ncolors = NULL, xlim = NULL, ...) {
     invisible(chart)
 }
 
-mychart4 <- function(data, splitfactor, orderfun = NULL, legendpos = "bottomright",
-    legendcol = "benchmark.name", column = "Values_per_cycle", xlab = "Values per Cycle", ...)
+mychart4 <- function(data, splitfactor, orderfun = function(d) { order(d[[medianCol]]) }, legendpos = "bottomright",
+    legendcol = "benchmark.name", column = "Values_per_cycle", xlab = "Values per Cycle", offsetInY = TRUE, ...)
 {
+    colorkey <- if(is.null(data$benchmark.arch)) data$benchmark.name else data$benchmark.arch
+    colorkey <- factor(colorkey)
+    ncolors <- nlevels(colorkey)
+    if(is.null(data$color  )) data$color   <- rainbow(ncolors, v = 0.5)[as.integer(colorkey)]
+    if(is.null(data$lcolor )) data$lcolor  <- {
+            tmp <- rgb2hsv(col2rgb(data$color))
+            hsv(h=tmp[row(tmp) == 1], s=tmp[row(tmp) == 2], v=0.8 * tmp[row(tmp) == 3], alpha=0.5)
+        }
     medianCol <- paste(column, ".median", sep="")
     meanCol   <- paste(column, ".mean"  , sep="")
     stddevCol <- paste(column, ".stddev", sep="")
-    if(is.null(orderfun)) orderfun <- function(d) { order(d[[medianCol]]) }
     chart <- NULL
     legendtext <- rep("unknown", times=50)
     xlim <- range(0, data[medianCol], data[meanCol] + data[stddevCol])
-    for(d in split(data, splitfactor)) {
+    splitted <- split(data, splitfactor)
+    wrap <- (length(splitted) - 1) * 0.5
+    steps <- if(offsetInY) 0.8 / length(splitted) else 0
+    for(d in splitted) {
         legendtext[d$pch[[1]]] <- as.character(d[[legendcol]][[1]])
         if(is.null(chart)) {
             permutation <- orderfun(d)
             d <- permute(d, permutation)
-            chart <- mychart3(d, column, xlab = xlab, xlim = xlim, ...)
+            chart <- mychart3(d, column, xlab = xlab, xlim = xlim, yoffset = steps * wrap, ...)
         } else {
             d <- permute(d, permutation)
+            chart$y <- chart$y - steps
             addPoints2(chart, d, column)
+            if(offsetInY) abline(h = chart$y, lty = "dotted", col = d$lcolor)
         }
     }
     pchLevels <- as.numeric(levels(factor(data$pch)))
-    legend(legendpos, legendtext[pchLevels], pch=pchLevels)
+    pchLevels <- c(pchLevels[pchLevels > 20], pchLevels[pchLevels <= 20])
+    cex <- par("cex")
+    if(cex >= 0.8) cex <- max(0.75, cex * 0.8)
+    legend(legendpos, legendtext[pchLevels], pch=pchLevels, cex = cex)
     invisible(chart)
 }
 
@@ -226,7 +241,23 @@ sortkey <- function(string, values, keys) {
     string + values
 }
 
-processData <- function(data, keys, skip = c("")) {
+permute <- function(data, o) {
+    if(is.data.frame(data)) {
+        for(col in colnames(data)) {
+            data[col] <- data[[col]][o]
+        }
+    } else {
+        data <- data[o]
+    }
+    data
+}
+
+sortBy <- function(data, key) {
+    o <- sort.list(key)
+    permute(data, o)
+}
+
+processData <- function(data, keys, skip = c(""), pchkey = NULL, sortkey = NULL, colorkey = NULL) {
     keys <- factor(keys) # no empty levels
     l <- levels(keys)
     n <- length(l)
@@ -247,23 +278,80 @@ processData <- function(data, keys, skip = c("")) {
             result[paste(col, sep=".", "stddev")] <- as.vector(tapply(v, keys, sd))
         }
     }
+    if(!is.null(pchkey)) {
+        result$pch <- c(21:25, 3:20)[as.integer(factor(result[[pchkey]]))]
+    }
+    if(!is.null(colorkey)) {
+        tmp <- factor(result[[colorkey]])
+        result$color <- rainbow(nlevels(tmp), v = 0.5)[as.integer(tmp)]
+    }
+    if(!is.null(sortkey)) {
+        result <- sortBy(result, result[[sortkey]])
+    }
     result
 }
 
-permute <- function(data, o) {
-    if(is.data.frame(data)) {
-        for(col in colnames(data)) {
-            data[col] <- data[[col]][o]
-        }
-    } else {
-        data <- data[o]
-    }
-    data
-}
+plotSpeedup <- function(sse, simple, lrb = data.frame(), datafun, plotfun = mychart4, main,
+    orderfun = function(d) order(d$datatype, d$benchmark.name), speedupColumn = NULL)
+{
+    lrb    <- split(lrb   , lrb$datatype)
+    sse    <- split(sse   , sse$datatype)
+    simple <- split(simple, simple$datatype)
 
-sortBy <- function(data, key) {
-    o <- sort.list(key)
-    permute(data, o)
+    speedupOf <- function(data, reference) {
+        tmp <- datafun(data, reference)
+        if(is.null(tmp$datatype) && !is.null(data$datatype)) tmp$datatype <- data$datatype
+        if(is.null(tmp$benchmark.name) && !is.null(data$benchmark.name)) tmp$benchmark.name <- data$benchmark.name
+        if(is.null(tmp$pch) && !is.null(data$pch)) tmp$pch <- data$pch
+        if(is.null(tmp$color) && !is.null(data$color)) tmp$color <- data$color
+        if(is.null(tmp$speedup) && !is.null(speedupColumn)) {
+            medianCol <- paste(speedupColumn, ".median", sep="")
+            meanCol   <- paste(speedupColumn, ".mean"  , sep="")
+            stddevCol <- paste(speedupColumn, ".stddev", sep="")
+            tmp$speedup.median <- data[[medianCol]] / reference[[medianCol]]
+            tmp$speedup.mean <- data[[meanCol]] / reference[[meanCol]]
+            tmp$speedup.stddev <- sqrt(
+                    (data[[stddevCol]] / data[[meanCol]]) ^ 2 +
+                    (reference[[stddevCol]] / reference[[meanCol]]) ^ 2
+                ) * data[[meanCol]] / reference[[meanCol]]
+        }
+        if(is.null(tmp$split)) tmp$split <- tmp$benchmark.name
+        data.frame(
+            tmp,
+            stringsAsFactors = FALSE
+            )
+    }
+
+    speedup <- rbind(
+        speedupOf(sse[["float_v"]], simple[["float_v"]]),
+        speedupOf(sse[["sfloat_v"]], simple[["float_v"]]),
+        speedupOf(sse[["short_v"]], simple[["short_v"]])
+        )
+
+    plotfun(speedup, splitfactor = speedup$split, orderfun = orderfun,
+        column = "speedup", xlab = "Speedup",
+        main = paste(main, ": SSE vs. Scalar", sep=""))
+    abline(v = 1, lty = "dashed", col = hsv(s = 1, v = 0, alpha = 0.4))
+
+    speedup <- rbind(
+        speedupOf(lrb[["float_v"]], simple[["float_v"]]),
+        speedupOf(lrb[["short_v"]], simple[["short_v"]])
+        )
+
+    plotfun(speedup, splitfactor = speedup$split, orderfun = orderfun,
+        column = "speedup", xlab = "Speedup",
+        main = paste(main, ": LRB Prototype vs. Scalar", sep=""))
+    abline(v = 1, lty = "dashed", col = hsv(s = 1, v = 0, alpha = 0.4))
+
+    speedup <- rbind(
+        speedupOf(lrb[["float_v"]], sse[["sfloat_v"]]),
+        speedupOf(lrb[["short_v"]], sse[["short_v"]])
+        )
+
+    plotfun(speedup, splitfactor = speedup$split, orderfun = orderfun,
+        column = "speedup", xlab = "Speedup",
+        main = paste(main, ": LRB Prototype vs. SSE", sep=""))
+    abline(v = 1, lty = "dashed", col = hsv(s = 1, v = 0, alpha = 0.4))
 }
 
 par(family="serif")
