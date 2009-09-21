767e520f4bb44d5e69abbd1f99ad3be13344d868
Matthias Kretz <Matthias.Kretz@kip.uni-heidelberg.de>
2009-09-21 17:24:57 +0200

    cleanup

--- benchmarks/flops.r
+++ benchmarks/flops.r
@@ -14,14 +14,6 @@ for(data in list(rbind(sse, simple, lrb))) {
     mychart2(FLOP_per_cycle, keys, xlab="FLOP per Cycle", main="Multiply - Add", groups=n:1, lcolor=lcolors, bg=bgs, color=colors, pch=pch)
     mychart2(GFLOP_per_sec_Real, keys, xlab="GFLOP per Second", main="Multiply - Add", groups=n:1, lcolor=lcolors, bg=bgs, color=colors, pch=pch)
 
-#    maxx <- ceiling(max(FLOP_per_cycle))
-#    dotchart( tapply(FLOP_per_cycle, benchmark.name, median), groups=groups, xlim=c(0,maxx), xaxs="i", xlab="FLOP per cycle", main="multiply - add", sub="median")
-#    dotchart( tapply(FLOP_per_cycle, benchmark.name, relerr), groups=groups,                 xaxs="i", xlab="FLOP per cycle", main="multiply - add", sub="relative error")
-#
-#    FLOP_per_sec_Real <- FLOP_per_sec_Real / 1000000000
-#    maxx <- ceiling(max(FLOP_per_sec_Real))
-#    dotchart( tapply(FLOP_per_sec_Real, benchmark.name, median), groups=groups, xlim=c(0,maxx), xlab="GFLOP per second", main="multiply - add", sub="median")
-#    dotchart( tapply(FLOP_per_sec_Real, benchmark.name, relerr), groups=groups,                 xlab="GFLOP per second", main="multiply - add", sub="relative error")
     detach(data)
 }
 
