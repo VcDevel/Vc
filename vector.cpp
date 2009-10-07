b6cdc6d81fc46c38f87947c4af44bf48763c660b
Matthias Kretz <Matthias.Kretz@kip.uni-heidelberg.de>
2009-10-07 14:14:11 +0200

    reorder constants for better locality
    document which constants should share a cacheline (doesn't really work
    because the linker only supports 16 byte alignment)

--- vector.cpp
+++ vector.cpp
@@ -25,28 +25,33 @@
 # endif
 #endif
 
-#include <cmath>
+#include <cmath> // for M_PI
 #include "sse/const.h"
 
 namespace Vc
 {
 namespace SSE
 {
-    V_ALIGN(16) extern const unsigned int   _IndexesFromZero4[4] = { 0, 1, 2, 3 };
-    V_ALIGN(16) extern const unsigned short _IndexesFromZero8[8] = { 0, 1, 2, 3, 4, 5, 6, 7 };
-
+    // cacheline 1
     V_ALIGN(16) const unsigned int c_general::allone[4] = { 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff };
-    V_ALIGN(16) const unsigned short c_general::one16[8] = { 1, 1, 1, 1, 1, 1, 1, 1 };
-    V_ALIGN(16) const unsigned int c_general::one32[4] = { 1, 1, 1, 1 };
-    V_ALIGN(16) const float c_general::oneFloat[4] = { 1.f, 1.f, 1.f, 1.f };
-    V_ALIGN(16) const double c_general::oneDouble[2] = { 1., 1. };
     V_ALIGN(16) const int c_general::absMaskFloat[4] = { 0x7fffffff, 0x7fffffff, 0x7fffffff, 0x7fffffff };
-    V_ALIGN(16) const long long c_general::absMaskDouble[2] = { 0x7fffffffffffffff, 0x7fffffffffffffff };
     V_ALIGN(16) const unsigned int c_general::signMaskFloat[4] = { 0x80000000, 0x80000000, 0x80000000, 0x80000000 };
-    V_ALIGN(16) const unsigned long long c_general::signMaskDouble[2] = { 0x8000000000000000, 0x8000000000000000 };
     V_ALIGN(16) const short c_general::minShort[8] = { -0x8000, -0x8000, -0x8000, -0x8000, -0x8000, -0x8000, -0x8000, -0x8000 };
 
+    // cacheline 2
+    V_ALIGN(16) extern const unsigned int   _IndexesFromZero4[4] = { 0, 1, 2, 3 };
+    V_ALIGN(16) extern const unsigned short _IndexesFromZero8[8] = { 0, 1, 2, 3, 4, 5, 6, 7 };
+    V_ALIGN(16) const unsigned short c_general::one16[8] = { 1, 1, 1, 1, 1, 1, 1, 1 };
+    V_ALIGN(16) const unsigned int c_general::one32[4] = { 1, 1, 1, 1 };
+
+    // cacheline 3
+    V_ALIGN(16) const float c_general::oneFloat[4] = { 1.f, 1.f, 1.f, 1.f };
+    V_ALIGN(16) const double c_general::oneDouble[2] = { 1., 1. };
+    V_ALIGN(16) const long long c_general::absMaskDouble[2] = { 0x7fffffffffffffff, 0x7fffffffffffffff };
+    V_ALIGN(16) const unsigned long long c_general::signMaskDouble[2] = { 0x8000000000000000, 0x8000000000000000 };
+
     template<> const float c_sin<float>::_data[4 * 8] = {
+    // cacheline 4
         // 1 over 2pi
         1.59154936671257019e-01f, 1.59154936671257019e-01f, 1.59154936671257019e-01f, 1.59154936671257019e-01f,
         // 2pi
@@ -56,6 +61,7 @@ namespace SSE
         // pi
         3.14159274101257324f, 3.14159274101257324f, 3.14159274101257324f, 3.14159274101257324f,
 
+    // cacheline 5
         // 1 over 3!
         1.66666671633720398e-01f, 1.66666671633720398e-01f, 1.66666671633720398e-01f, 1.66666671633720398e-01f,
         // 1 over 5!
@@ -67,6 +73,7 @@ namespace SSE
     };
 
     template<> const double c_sin<double>::_data[2 * 8] = {
+    // cacheline 6
         // 1 over 2pi
         0.5 / M_PI, 0.5 / M_PI,
         // 2pi
@@ -76,6 +83,7 @@ namespace SSE
         // pi
         M_PI, M_PI,
 
+    // cacheline 7
         // 1 over 3!
         1.666666666666666574148081281236954964697360992431640625e-01, 1.666666666666666574148081281236954964697360992431640625e-01,
         // 1 over 5!
@@ -89,6 +97,7 @@ namespace SSE
 
 namespace LRBni
 {
+    // cacheline 8
     V_ALIGN(16) extern const char _IndexesFromZero[16] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 } // namespace Larrabee
 } // namespace Vc
