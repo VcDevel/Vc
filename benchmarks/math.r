5cee5997b79ec9bbc758f6ecdd9920e2111018c7
Matthias Kretz <Matthias.Kretz@kip.uni-heidelberg.de>
2009-10-12 19:37:34 +0200

    color by datatype; no xlab offset necessary

--- benchmarks/math.r
+++ benchmarks/math.r
@@ -1,6 +1,6 @@
 mathProcessData <- function(d) {
     d <- processData(d, paste(d$datatype, d$benchmark.arch, d$benchmark.name),
-        pchkey = "benchmark.name")
+        pchkey = "benchmark.name", colorkey = "datatype")
     if(length(d) > 0) d$key <- paste(d$datatype, d$benchmark.arch)
     d
 }
@@ -18,7 +18,7 @@ data <- rbind(sse, simple, lrb)
 for(data in list(set1(data), set2(data))) {
     data <- mathProcessData(data)
     mychart4(data, data$benchmark.name, column = "Op_per_cycle", xlab = "Operations per Cycle",
-        main = "Math Operations", orderfun = function(d) order(d$key))
+        main = "Math Operations", orderfun = function(d) order(d$key), maxlaboffset = 1)
 }
 
 lrb1    <- mathProcessData(set1(lrb))
@@ -29,10 +29,10 @@ sse2    <- mathProcessData(set2(sse))
 simple2 <- mathProcessData(set2(simple))
 
 plotSpeedup(sse1, simple1, lrb1, main = "Math Operations", speedupColumn = "Op_per_cycle",
-    datafun = function(d, ref) list(key = d$datatype)
+    datafun = function(d, ref) list(key = d$datatype), maxlaboffset = 1
     )
 plotSpeedup(sse2, simple2, lrb2, main = "Math Operations", speedupColumn = "Op_per_cycle",
-    datafun = function(d, ref) list(key = d$datatype)
+    datafun = function(d, ref) list(key = d$datatype), maxlaboffset = 1
     )
 
 
