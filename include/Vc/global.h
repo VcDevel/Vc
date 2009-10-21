976f8cc076e6f2fab58ad4c929fd9cd94fd61a64
Matthias Kretz <Matthias.Kretz@kip.uni-heidelberg.de>
2009-10-21 21:23:47 +0200

    fix global.h to define all necessary macros

--- include/Vc/global.h
+++ include/Vc/global.h
@@ -20,6 +20,10 @@
 #ifndef VC_IMPL
 # define VC_IMPL
 
+# if defined(__SSE4_2__)
+#  define VC_IMPL_SSE 1
+#  define VC_IMPL_SSE4_2 1
+# endif
 # if defined(__SSE4_1__)
 #  define VC_IMPL_SSE 1
 #  define VC_IMPL_SSE4_1 1
@@ -48,45 +52,76 @@
 #else // VC_IMPL
 
 #define SSE    9875294
-
-# if VC_IMPL == SSE
-#  undef VC_IMPL
-#  if defined(__SSE4_1__)
-#   define VC_IMPL_SSE 1
-#   define VC_IMPL_SSE4_1 1
-#   define VC_IMPL SSE4_1
-#  elif defined(__SSSE3__)
-#   define VC_IMPL_SSE 1
-#   define VC_IMPL_SSSE3 1
-#   define VC_IMPL SSSE3
-#  elif defined(__SSE3__)
-#   define VC_IMPL_SSE 1
-#   define VC_IMPL_SSE3 1
-#   define VC_IMPL SSE3
-#  elif defined(__SSE2__)
-#   define VC_IMPL_SSE 1
-#   define VC_IMPL_SSE2 1
-#   define VC_IMPL SSE2
-#  else
-#   define VC_IMPL_SSE 1
-#   define VC_IMPL_SSE4_1 1
-#   define VC_IMPL SSE4_1
-#  endif
-# endif
-
 #define SSE2   9875295
 #define SSE3   9875296
 #define SSSE3  9875297
 #define SSE4_1 9875298
 #define Scalar 9875299
 #define LRBni  9875300
+#define SSE4_2 9875301
+
+# if VC_IMPL == SSE4_2
+#  define VC_IMPL_SSE4_2 1
+#  define VC_IMPL_SSE4_1 1
+#  define VC_IMPL_SSSE3 1
+#  define VC_IMPL_SSE3 1
+#  define VC_IMPL_SSE2 1
+#  define VC_IMPL_SSE 1
+# endif
+# if VC_IMPL == SSE4_1
+#  define VC_IMPL_SSE4_1 1
+#  define VC_IMPL_SSSE3 1
+#  define VC_IMPL_SSE3 1
+#  define VC_IMPL_SSE2 1
+#  define VC_IMPL_SSE 1
+# endif
+# if VC_IMPL == SSSE3
+#  define VC_IMPL_SSSE3 1
+#  define VC_IMPL_SSE3 1
+#  define VC_IMPL_SSE2 1
+#  define VC_IMPL_SSE 1
+# endif
+# if VC_IMPL == SSE3
+#  define VC_IMPL_SSE3 1
+#  define VC_IMPL_SSE2 1
+#  define VC_IMPL_SSE 1
+# endif
+# if VC_IMPL == SSE2
+#  define VC_IMPL_SSE2 1
+#  define VC_IMPL_SSE 1
+# endif
+
+# if VC_IMPL == SSE
+#  define VC_IMPL_SSE 1
+#  if defined(__SSE4_2__)
+#   define VC_IMPL_SSE4_2 1
+#  endif
+#  if defined(__SSE4_1__)
+#   define VC_IMPL_SSE4_1 1
+#  endif
+#  if defined(__SSE3__)
+#   define VC_IMPL_SSE3 1
+#  endif
+#  if defined(__SSSE3__)
+#   define VC_IMPL_SSSE3 1
+#  endif
+#  if defined(__SSE2__)
+#   define VC_IMPL_SSE2 1
+#  endif
+# endif
 
 # if VC_IMPL == Scalar
 #  define VC_IMPL_Scalar 1
 # elif VC_IMPL == LRBni
 #  define VC_IMPL_LRBni 1
-# elif VC_IMPL != SSE2 && VC_IMPL != SSE3 && VC_IMPL != SSSE3 && VC_IMPL != SSE4_1
-#  error "VC_IMPL is not set to a known value!"
+# endif
+
+# undef VC_IMPL
+
+# if !defined(VC_IMPL_LRBni) && !defined(VC_IMPL_Scalar) && !defined(VC_IMPL_SSE)
+#  error "No suitable Vc implementation was selected! Probably VC_IMPL was set to an unknown value."
+# elif defined(VC_IMPL_SSE) && !defined(VC_IMPL_SSE2)
+#  error "SSE requested but no SSE2 support. Vc needs at least SSE2!"
 # endif
 
 #undef SSE
@@ -94,6 +129,7 @@
 #undef SSE3
 #undef SSSE3
 #undef SSE4_1
+#undef SSE4_2
 #undef Scalar
 #undef LRBni
 
