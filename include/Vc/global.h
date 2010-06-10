5a6378cdb206f70d8ebd9aa893f140f4f129b8a6
Matthias Kretz <kretz@kde.org>
2010-06-10 18:51:32 +0200

    change global.h to pick up AVX and all SSE versions

--- include/Vc/global.h
+++ include/Vc/global.h
@@ -1,6 +1,6 @@
 /*  This file is part of the Vc library.
 
-    Copyright (C) 2009 Matthias Kretz <kretz@kde.org>
+    Copyright (C) 2009-2010 Matthias Kretz <kretz@kde.org>
 
     Vc is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as
@@ -28,34 +28,37 @@
 #define Scalar 9875299
 #define LRBni  9875300
 #define SSE4_2 9875301
+#define SSE4a  9875302
+#define AVX    9875303
 
 #ifndef VC_IMPL
 
-#  if defined(__GNUC__) && (__GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 3))
-#    ifndef VC_DONT_WARN_OLD_GCC
-#      warning "GCC < 4.3 does not have full support for SSE2 intrinsics. Using scalar types/operations only. Define VC_DONT_WARN_OLD_GCC to silence this warning."
-#    endif
-#  else
-#    if defined(__SSE4_2__)
-#      define VC_IMPL_SSE 1
-#      define VC_IMPL_SSE4_2 1
-#    endif
-#    if defined(__SSE4_1__)
-#      define VC_IMPL_SSE 1
-#      define VC_IMPL_SSE4_1 1
-#    endif
-#    if defined(__SSE3__)
-#      define VC_IMPL_SSE 1
-#      define VC_IMPL_SSE3 1
-#    endif
-#    if defined(__SSSE3__)
-#      define VC_IMPL_SSE 1
-#      define VC_IMPL_SSSE3 1
-#    endif
-#    if defined(__SSE2__)
-#      define VC_IMPL_SSE 1
-#      define VC_IMPL_SSE2 1
-#    endif
+#  if defined(__AVX__)
+#    define VC_IMPL_AVX 1
+#  endif
+#  if defined(__SSE4a__)
+#    define VC_IMPL_SSE 1
+#    define VC_IMPL_SSE4a 1
+#  endif
+#  if defined(__SSE4_2__)
+#    define VC_IMPL_SSE 1
+#    define VC_IMPL_SSE4_2 1
+#  endif
+#  if defined(__SSE4_1__)
+#    define VC_IMPL_SSE 1
+#    define VC_IMPL_SSE4_1 1
+#  endif
+#  if defined(__SSE3__)
+#    define VC_IMPL_SSE 1
+#    define VC_IMPL_SSE3 1
+#  endif
+#  if defined(__SSSE3__)
+#    define VC_IMPL_SSE 1
+#    define VC_IMPL_SSSE3 1
+#  endif
+#  if defined(__SSE2__)
+#    define VC_IMPL_SSE 1
+#    define VC_IMPL_SSE2 1
 #  endif
 
 #  if defined(VC_IMPL_SSE)
@@ -68,71 +71,88 @@
 
 #else // VC_IMPL
 
-# if VC_IMPL == SSE4_2
-#  define VC_IMPL_SSE4_2 1
-#  define VC_IMPL_SSE4_1 1
-#  define VC_IMPL_SSSE3 1
-#  define VC_IMPL_SSE3 1
-#  define VC_IMPL_SSE2 1
-#  define VC_IMPL_SSE 1
-# endif
-# if VC_IMPL == SSE4_1
-#  define VC_IMPL_SSE4_1 1
-#  define VC_IMPL_SSSE3 1
-#  define VC_IMPL_SSE3 1
-#  define VC_IMPL_SSE2 1
-#  define VC_IMPL_SSE 1
-# endif
-# if VC_IMPL == SSSE3
-#  define VC_IMPL_SSSE3 1
-#  define VC_IMPL_SSE3 1
-#  define VC_IMPL_SSE2 1
-#  define VC_IMPL_SSE 1
-# endif
-# if VC_IMPL == SSE3
-#  define VC_IMPL_SSE3 1
-#  define VC_IMPL_SSE2 1
-#  define VC_IMPL_SSE 1
-# endif
-# if VC_IMPL == SSE2
-#  define VC_IMPL_SSE2 1
-#  define VC_IMPL_SSE 1
-# endif
+#  if VC_IMPL == AVX // AVX supersedes SSE
+#    define VC_IMPL_AVX 1
+#  elif VC_IMPL == Scalar
+#    define VC_IMPL_Scalar 1
+#  elif VC_IMPL == LRBni
+#    define VC_IMPL_LRBni 1
+#  elif VC_IMPL == SSE4a
+#    define VC_IMPL_SSE4a 1
+#    define VC_IMPL_SSE3 1
+#    define VC_IMPL_SSE2 1
+#    define VC_IMPL_SSE 1
+#  elif VC_IMPL == SSE4_2
+#    define VC_IMPL_SSE4_2 1
+#    define VC_IMPL_SSE4_1 1
+#    define VC_IMPL_SSSE3 1
+#    define VC_IMPL_SSE3 1
+#    define VC_IMPL_SSE2 1
+#    define VC_IMPL_SSE 1
+#  elif VC_IMPL == SSE4_1
+#    define VC_IMPL_SSE4_1 1
+#    define VC_IMPL_SSSE3 1
+#    define VC_IMPL_SSE3 1
+#    define VC_IMPL_SSE2 1
+#    define VC_IMPL_SSE 1
+#  elif VC_IMPL == SSSE3
+#    define VC_IMPL_SSSE3 1
+#    define VC_IMPL_SSE3 1
+#    define VC_IMPL_SSE2 1
+#    define VC_IMPL_SSE 1
+#  elif VC_IMPL == SSE3
+#    define VC_IMPL_SSE3 1
+#    define VC_IMPL_SSE2 1
+#    define VC_IMPL_SSE 1
+#  elif VC_IMPL == SSE2
+#    define VC_IMPL_SSE2 1
+#    define VC_IMPL_SSE 1
+#  elif VC_IMPL == SSE
+#    define VC_IMPL_SSE 1
+#    if defined(__SSE4a__)
+#      define VC_IMPL_SSE4a 1
+#    endif
+#    if defined(__SSE4_2__)
+#      define VC_IMPL_SSE4_2 1
+#    endif
+#    if defined(__SSE4_1__)
+#      define VC_IMPL_SSE4_1 1
+#    endif
+#    if defined(__SSE3__)
+#      define VC_IMPL_SSE3 1
+#    endif
+#    if defined(__SSSE3__)
+#      define VC_IMPL_SSSE3 1
+#    endif
+#    if defined(__SSE2__)
+#      define VC_IMPL_SSE2 1
+#    endif
+#  endif
+#  undef VC_IMPL
 
-# if VC_IMPL == SSE
-#  define VC_IMPL_SSE 1
-#  if defined(__SSE4_2__)
-#   define VC_IMPL_SSE4_2 1
-#  endif
-#  if defined(__SSE4_1__)
-#   define VC_IMPL_SSE4_1 1
-#  endif
-#  if defined(__SSE3__)
-#   define VC_IMPL_SSE3 1
-#  endif
-#  if defined(__SSSE3__)
-#   define VC_IMPL_SSSE3 1
-#  endif
-#  if defined(__SSE2__)
-#   define VC_IMPL_SSE2 1
-#  endif
-# endif
+#endif // VC_IMPL
 
-# if VC_IMPL == Scalar
-#  define VC_IMPL_Scalar 1
-# elif VC_IMPL == LRBni
-#  define VC_IMPL_LRBni 1
-# endif
+#if defined(__GNUC__) && (__GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 3))
+#  ifndef VC_DONT_WARN_OLD_GCC
+#    warning "GCC < 4.3 does not have full support for SSE2 intrinsics. Using scalar types/operations only. Define VC_DONT_WARN_OLD_GCC to silence this warning."
+#  endif
+#  undef VC_IMPL_SSE
+#  undef VC_IMPL_SSE2
+#  undef VC_IMPL_SSE3
+#  undef VC_IMPL_SSE4a
+#  undef VC_IMPL_SSE4_1
+#  undef VC_IMPL_SSE4_2
+#  undef VC_IMPL_SSSE3
+#  undef VC_IMPL_AVX
+#  undef VC_IMPL_LRBni
+#  define VC_IMPL_Scalar
+#endif
 
-# undef VC_IMPL
-
-# if !defined(VC_IMPL_LRBni) && !defined(VC_IMPL_Scalar) && !defined(VC_IMPL_SSE)
-#  error "No suitable Vc implementation was selected! Probably VC_IMPL was set to an unknown value."
+# if !defined(VC_IMPL_LRBni) && !defined(VC_IMPL_Scalar) && !defined(VC_IMPL_SSE) && !defined(VC_IMPL_AVX)
+#  error "No suitable Vc implementation was selected! Probably VC_IMPL was set to an invalid value."
 # elif defined(VC_IMPL_SSE) && !defined(VC_IMPL_SSE2)
 #  error "SSE requested but no SSE2 support. Vc needs at least SSE2!"
 # endif
-
-#endif // VC_IMPL
 
 #undef SSE
 #undef SSE2
@@ -140,6 +160,8 @@
 #undef SSSE3
 #undef SSE4_1
 #undef SSE4_2
+#undef SSE4a
+#undef AVX
 #undef Scalar
 #undef LRBni
 
