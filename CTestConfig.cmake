56b7bd1b9424e79e49a9e3458ac775ffac92e0b7
Matthias Kretz <kretz@kde.org>
2010-06-11 18:00:20 +0200

    submit to dashboard at GSI

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,6 +1,6 @@
 SET(CTEST_DROP_METHOD "http")
-SET(CTEST_DROP_SITE "my.cdash.org")
-SET(CTEST_DROP_LOCATION "/submit.php?project=Vc")
+SET(CTEST_DROP_SITE "fairroot.gsi.de")
+SET(CTEST_DROP_LOCATION "/CDash/submit.php?project=VC")
 SET(CTEST_UPDATE_TYPE git)
 
 find_program(GITCOMMAND git)
