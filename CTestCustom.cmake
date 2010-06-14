f561177732b9f00d9f5321ec4bd79903690f1f61
Matthias Kretz <kretz@kde.org>
2010-06-14 08:04:38 +0200

    ignore incorrect warnings

---
+++ CTestCustom.cmake
@@ -0,0 +1,1 @@
+set(CTest_CUSTOM_WARNING_EXCEPTION "(expandandmerge.cpp:(41|64|76)|gather.cpp:(39|40)|load.cpp:(65|90)): warning: ...(c|ii|castedMask|b)... (is|may be) used uninitialized in this function")
