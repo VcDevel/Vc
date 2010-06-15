1cf55d54f6aa6154d2781d332cfab1924c580cfc
Matthias Kretz <kretz@kde.org>
2010-06-15 16:16:11 +0200

    silence the trigger site error

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -2,6 +2,7 @@ SET(CTEST_DROP_METHOD "http")
 SET(CTEST_DROP_SITE "fairroot.gsi.de")
 SET(CTEST_DROP_LOCATION "/CDash/submit.php?project=VC")
 SET(CTEST_UPDATE_TYPE git)
+SET(CTEST_TRIGGER_SITE "fairroot.gsi.de")
 
 find_program(GITCOMMAND git)
 mark_as_advanced(GITCOMMAND)
