f5d4d33ffbe07b3657b66158f2449bc31d5472d6
Matthias Kretz <kretz@kde.org>
2010-06-15 12:20:52 +0200

    ignore noise about #warning

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -1,1 +1,2 @@
-set(CTest_CUSTOM_WARNING_EXCEPTION "(expandandmerge.cpp:(41|64|76)|gather.cpp:(39|40)|load.cpp:(65|90)): warning: ...(c|ii|castedMask|b)... (is|may be) used uninitialized in this function")
+set(CTest_CUSTOM_WARNING_EXCEPTION ${CTest_CUSTOM_WARNING_EXCEPTION} "(expandandmerge.cpp:(41|64|76)|gather.cpp:(39|40)|load.cpp:(65|90)): warning: ...(c|ii|castedMask|b)... (is|may be) used uninitialized in this function")
+set(CTest_CUSTOM_WARNING_EXCEPTION ${CTest_CUSTOM_WARNING_EXCEPTION} "warning is a GCC extension")
