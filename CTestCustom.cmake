513a8e47e07ed6d5edc11ee59d84170c2b656632
Matthias Kretz <kretz@kde.org>
2010-06-15 16:44:40 +0200

    fix warning suppression

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -1,2 +1,4 @@
-set(CTest_CUSTOM_WARNING_EXCEPTION ${CTest_CUSTOM_WARNING_EXCEPTION} "(expandandmerge.cpp:(41|64|76)|gather.cpp:(39|40)|load.cpp:(65|90)): warning: ...(c|ii|castedMask|b)... (is|may be) used uninitialized in this function")
-set(CTest_CUSTOM_WARNING_EXCEPTION ${CTest_CUSTOM_WARNING_EXCEPTION} "warning is a GCC extension")
+set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION} "expandandmerge.cpp:(41|64|76): warning: .+c.+ (is|may be) used uninitialized in this function")
+set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION} "gather.cpp:(39|40): warning: .+(ii|castedMask).+ (is|may be) used uninitialized in this function")
+set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION} "load.cpp:(65|90): warning: .+b.+ (is|may be) used uninitialized in this function")
+set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION} "warning is a GCC extension")
