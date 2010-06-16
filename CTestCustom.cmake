652e0f721fc1037061cb26e5e1b3a89742379d4f
Matthias Kretz <kretz@kde.org>
2010-06-16 19:04:19 +0200

    GCC gives even more noise to suppress

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -1,4 +1,5 @@
 set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION} "expandandmerge.cpp:(41|64|76): warning: .+c.+ (is|may be) used uninitialized in this function")
-set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION} "gather.cpp:(39|40): warning: .+(ii|castedMask).+ (is|may be) used uninitialized in this function")
+set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION} "gather.cpp:(39|40|42|44): warning: .+(a|b|ii|castedMask).+ (is|may be) used uninitialized in this function")
 set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION} "load.cpp:(65|90): warning: .+b.+ (is|may be) used uninitialized in this function")
+set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION} "used uninitialized in this function")
 set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION} "warning is a GCC extension")
