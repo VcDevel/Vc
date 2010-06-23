b4e41ccc6eb80df3cb07e813fe701c5f52ff47c0
Matthias Kretz <kretz@kde.org>
2010-06-23 09:41:58 +0200

    ignore uninteresting error lines from make

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -1,3 +1,8 @@
 set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION} "used uninitialized in this function")
 set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION} "warning is a GCC extension")
 set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION} "^-- ") # Ignore output from cmake
+set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION} "^\\*\\*\\* WARNING non-zero return value in ctest from: make") # Ignore output from ctest
+
+set(CTEST_CUSTOM_ERROR_EXCEPTION ${CTEST_CUSTOM_ERROR_EXCEPTION} "^make\\[[1-9]\\]: ")
+set(CTEST_CUSTOM_ERROR_EXCEPTION ${CTEST_CUSTOM_ERROR_EXCEPTION} "^collect2: ld returned . exit status")
+set(CTEST_CUSTOM_ERROR_EXCEPTION ${CTEST_CUSTOM_ERROR_EXCEPTION} "^make: \\*\\*\\* \\[all\\] Error ")
