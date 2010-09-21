fb3afe2ef65edf76ff15ae1de1230b0a8c0da230
Matthias Kretz <kretz@kde.org>
2010-09-21 17:58:18 +0200

    fix spurious utils_sse failures by providing a more robust operator[]
    I'm not sure whether it was a GCC miscompilation or not. Basically the
    resulting code would first load a scalar value for comparison and only
    after that actually store the vector to the memory location it just read
    from. According to my understanding of __attribute__((may_alias)) this
    should not have happened. Now a helper object makes it a little clearer
    to the compiler what is going on.

--- sse/vector.h
+++ sse/vector.h
@@ -24,8 +24,11 @@
 #include "vectorbase.h"
 #include "vectorhelper.h"
 #include "mask.h"
+#include "../common/aliasingentryhelper.h"
 #include <algorithm>
 #include <cmath>
+
+#include "macros.h"
 
 #ifdef isfinite
 #undef isfinite
@@ -112,7 +115,6 @@ class Vector : public VectorBase<T>
         typedef VectorBase<T> Base;
         using Base::d;
         typedef typename Base::GatherMaskType GatherMask;
-        typedef typename Base::StorageType::AliasingEntryType AliasingEntryType;
     public:
         FREE_STORE_OPERATORS_ALIGNED(16)
 
@@ -324,7 +326,7 @@ class Vector : public VectorBase<T>
         //postfix
         inline Vector operator++(int) INTRINSIC { const Vector<T> r = *this; data() = VectorHelper<T>::add(data(), VectorHelper<T>::one()); return r; }
 
-        inline AliasingEntryType &operator[](int index) INTRINSIC {
+        inline Common::AliasingEntryHelper<EntryType> operator[](int index) INTRINSIC {
             return Base::d.m(index);
         }
         inline EntryType operator[](int index) const PURE INTRINSIC {
