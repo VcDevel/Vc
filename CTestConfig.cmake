69aed7769f08599a285384e422934efe7e928ef4
Matthias Kretz <kretz@kde.org>
2010-12-03 18:02:09 +0100

    use own project for the 0.4 branch dashboard

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,8 +1,11 @@
-SET(CTEST_DROP_METHOD "http")
-SET(CTEST_DROP_SITE "code.compeng.uni-frankfurt.de")
-SET(CTEST_DROP_LOCATION "/dashboard/submit.php?project=Vc")
-SET(CTEST_TRIGGER_SITE "${CTEST_DROP_SITE}")
-SET(CTEST_NIGHTLY_START_TIME "00:00:00 CET")
+set(CTEST_PROJECT_NAME "Vc-0.4")
+set(CTEST_NIGHTLY_START_TIME "00:00:00 CET")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "code.compeng.uni-frankfurt.de")
+set(CTEST_DROP_LOCATION "/dashboard/submit.php?project=Vc-0.4")
+set(CTEST_DROP_SITE_CDASH TRUE)
+
 SET(CTEST_UPDATE_TYPE "git")
 
 find_program(GITCOMMAND git)
