1ff7a9bb1bdb6b1e251d9c8b315da21ce1192aee
Matthias Kretz <kretz@kde.org>
2011-02-17 11:15:45 +0100

    mask class(es) for AVX. Using either 32 or 16 Bytes

--- avx/mask.h
+++ avx/mask.h
@@ -1,6 +1,6 @@
 /*  This file is part of the Vc library.
 
-    Copyright (C) 2009-2010 Matthias Kretz <kretz@kde.org>
+    Copyright (C) 2009-2011 Matthias Kretz <kretz@kde.org>
 
     Vc is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as
@@ -27,27 +27,14 @@ namespace Vc
 namespace AVX
 {
 
-template<unsigned int Size1> struct MaskHelper;
-template<> struct MaskHelper<2> {
-    static inline bool cmpeq (_M256 k1, _M256 k2) { return _mm256_movemask_pd(_mm256_castps_pd(k1)) == _mm256_movemask_pd(_mm256_castps_pd(k2)); }
-    static inline bool cmpneq(_M256 k1, _M256 k2) { return _mm256_movemask_pd(_mm256_castps_pd(k1)) != _mm256_movemask_pd(_mm256_castps_pd(k2)); }
-};
-template<> struct MaskHelper<4> {
-    static inline bool cmpeq (_M256 k1, _M256 k2) { return _mm256_movemask_ps(k1) == _mm256_movemask_ps(k2); }
-    static inline bool cmpneq(_M256 k1, _M256 k2) { return _mm256_movemask_ps(k1) != _mm256_movemask_ps(k2); }
-};
-template<> struct MaskHelper<8> {
-    static inline bool cmpeq (_M256 k1, _M256 k2) { return _mm256_movemask_epi8(_mm256_castps_si256(k1)) == _mm256_movemask_epi8(_mm256_castps_si256(k2)); }
-    static inline bool cmpneq(_M256 k1, _M256 k2) { return _mm256_movemask_epi8(_mm256_castps_si256(k1)) != _mm256_movemask_epi8(_mm256_castps_si256(k2)); }
-};
+template<unsigned int VectorSize, size_t RegisterWidth> class Mask;
 
-template<unsigned int VectorSize> class Mask
+template<unsigned int VectorSize> class Mask<VectorSize, 32u>
 {
-    friend class Mask<2u>;
-    friend class Mask<4u>;
-    friend class Mask<8u>;
-    friend class Mask<16u>;
-    friend class Mask<32u>;
+    friend class Mask<4u, 32u>; // double_v
+    friend class Mask<8u, 32u>; // float_v, (u)int_v
+    friend class Mask<8u, 16u>; // (u)short_v
+    friend class Mask<16u, 16u>; // (u)char_v
     public:
         FREE_STORE_OPERATORS_ALIGNED(32)
         inline Mask() {}
@@ -58,6 +45,11 @@ template<unsigned int VectorSize> class Mask
         inline explicit Mask(VectorSpecialInitializerOne::OEnum) : k(_mm256_setallone_ps()) {}
         inline explicit Mask(bool b) : k(b ? _mm256_setallone_ps() : _mm256_setzero_ps()) {}
         inline Mask(const Mask &rhs) : k(rhs.k) {}
+        inline Mask(const Mask<VectorSize, 16u> &rhs) : k(
+                _mm256_insertf128_ps(
+                    avx_cast<__m256>(_mm_unpacklo_epi16(rhs.data(), rhs.data())),
+                    avx_cast<__m128>(_mm_unpackhi_epi16(rhs.data(), rhs.data())),
+                    1)) {}
         inline Mask(const Mask<VectorSize / 2> *a)
           : k(_mm256_castsi256_ps(_mm256_packs_epi16(a[0].dataI(), a[1].dataI()))) {}
 
@@ -78,56 +70,33 @@ template<unsigned int VectorSize> class Mask
 
         void expand(Mask<VectorSize / 2> *x) const;
 
-        inline bool operator==(const Mask &rhs) const { return MaskHelper<VectorSize>::cmpeq (k, rhs.k); }
-        inline bool operator!=(const Mask &rhs) const { return MaskHelper<VectorSize>::cmpneq(k, rhs.k); }
+        inline bool operator==(const Mask &rhs) const { return 0 != _mm256_testc_ps(k, rhs.k); }
+        inline bool operator!=(const Mask &rhs) const { return 0 == _mm256_testc_ps(k, rhs.k); }
 
         inline Mask operator&&(const Mask &rhs) const { return _mm256_and_ps(k, rhs.k); }
         inline Mask operator& (const Mask &rhs) const { return _mm256_and_ps(k, rhs.k); }
         inline Mask operator||(const Mask &rhs) const { return _mm256_or_ps (k, rhs.k); }
         inline Mask operator| (const Mask &rhs) const { return _mm256_or_ps (k, rhs.k); }
         inline Mask operator^ (const Mask &rhs) const { return _mm256_xor_ps(k, rhs.k); }
-        inline Mask operator!() const { return _mm256_andnot_si256(dataI(), _mm256_setallone_si256()); }
+        inline Mask operator!() const { return _mm256_andnot_ps(data(), _mm256_setallone_ps()); }
 
         inline Mask &operator&=(const Mask &rhs) { k = _mm256_and_ps(k, rhs.k); return *this; }
         inline Mask &operator|=(const Mask &rhs) { k = _mm256_or_ps (k, rhs.k); return *this; }
 
-        inline bool isFull () const { return
-#ifdef VC_USE_PTEST
-            _mm256_testc_si256(dataI(), _mm256_setallone_si256()); // return 1 if (0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff) == (~0 & k)
-#else
-            _mm256_movemask_epi8(dataI()) == 0xffff;
-#endif
-        }
-        inline bool isEmpty() const { return
-#ifdef VC_USE_PTEST
-            _mm256_testz_si256(dataI(), dataI()); // return 1 if (0, 0, 0, 0) == (k & k)
-#else
-            _mm256_movemask_epi8(dataI()) == 0x0000;
-#endif
-        }
-        inline bool isMix() const {
-#ifdef VC_USE_PTEST
-            return _mm256_test_mix_ones_zeros(dataI(), _mm256_setallone_si256());
-#else
-            const int tmp = _mm256_movemask_epi8(dataI());
-            return tmp != 0 && (tmp ^ 0xffff) != 0;
-#endif
-        }
+        // no need for expression template optimizations because cmp(n)eq for floats are not bitwise
+        // compares
+        inline bool isFull () const { return 0 != _mm256_testc_ps(k, _mm256_setallone_ps()); }
+        inline bool isEmpty() const { return 0 != _mm256_testz_ps(k, k); }
+        inline bool isMix  () const { return 0 != _mm256_testnzc_ps(k, _mm256_setallone_ps()); }
 
         inline operator bool() const { return isFull(); }
 
         inline int shiftMask() const CONST;
-
         int toInt() const CONST;
 
         inline _M256  data () const { return k; }
-#ifdef VC_GATHER_SET
-        inline _M256  dataIndex() const { return k; }
-#endif
         inline _M256I dataI() const { return _mm256_castps_si256(k); }
         inline _M256D dataD() const { return _mm256_castps_pd(k); }
-
-        template<unsigned int OtherSize> inline Mask<OtherSize> cast() const { return Mask<OtherSize>(k); }
 
         bool operator[](int index) const;
 
@@ -144,16 +113,78 @@ template<unsigned int VectorSize> class Mask
         _M256 k;
 };
 
+template<unsigned int VectorSize> class Mask<VectorSize, 16u>
+{
+    friend class Mask<4u, 32u>; // double_v
+    friend class Mask<8u, 32u>; // float_v, (u)int_v
+    friend class Mask<8u, 16u>; // (u)short_v
+    friend class Mask<16u, 16u>; // (u)char_v
+    public:
+        FREE_STORE_OPERATORS_ALIGNED(16)
+        inline Mask() {}
+        inline Mask(const __m128  &x) : k(x) {}
+        inline Mask(const __m128d &x) : k(_mm_castpd_ps(x)) {}
+        inline Mask(const __m128i &x) : k(_mm_castsi128_ps(x)) {}
+        inline explicit Mask(VectorSpecialInitializerZero::ZEnum) : k(_mm_setzero_ps()) {}
+        inline explicit Mask(VectorSpecialInitializerOne::OEnum) : k(_mm_setallone_ps()) {}
+        inline explicit Mask(bool b) : k(b ? _mm_setallone_ps() : _mm_setzero_ps()) {}
+        inline Mask(const Mask &rhs) : k(rhs.k) {}
+        inline Mask(const Mask<VectorSize, 32u> &rhs) : k(avx_cast<__m128>(
+                _mm_packs_epi32(avx_cast<__m128i>(rhs.data()), _mm256_extractf128_si256(rhs.dataI(), 1)))) {}
+        inline Mask(const Mask<VectorSize / 2, 16u> *a) : k(avx_cast<__m128>(
+                _mm_packs_epi16(a[0].dataI(), a[1].dataI()))) {}
+
+        inline bool operator==(const Mask &rhs) const { return 0 != _mm_testc_si128(dataI(), rhs.dataI()); }
+        inline bool operator!=(const Mask &rhs) const { return 0 == _mm_testc_si128(dataI(), rhs.dataI()); }
+
+        inline Mask operator&&(const Mask &rhs) const { return _mm_and_ps(k, rhs.k); }
+        inline Mask operator& (const Mask &rhs) const { return _mm_and_ps(k, rhs.k); }
+        inline Mask operator||(const Mask &rhs) const { return _mm_or_ps (k, rhs.k); }
+        inline Mask operator| (const Mask &rhs) const { return _mm_or_ps (k, rhs.k); }
+        inline Mask operator^ (const Mask &rhs) const { return _mm_xor_ps(k, rhs.k); }
+        inline Mask operator!() const { return _mm_andnot_ps(data(), _mm_setallone_ps()); }
+
+        inline Mask &operator&=(const Mask &rhs) { k = _mm_and_ps(k, rhs.k); return *this; }
+        inline Mask &operator|=(const Mask &rhs) { k = _mm_or_ps (k, rhs.k); return *this; }
+
+        // TODO: use expression templates to optimize (v1 == v2).isFull() and friends
+        inline bool isFull () const { return 0 != _mm_testc_si128(dataI(), _mm_setallone_si128()); }
+        inline bool isEmpty() const { return 0 != _mm_testz_si128(dataI(), dataI()); }
+        inline bool isMix  () const { return 0 != _mm_testnzc_si128(dataI(), _mm_setallone_si128()); }
+
+        inline operator bool() const { return isFull(); }
+
+        inline int shiftMask() const CONST;
+        int toInt() const CONST;
+
+        inline __m128  data () const { return k; }
+        inline __m128i dataI() const { return avx_cast<__m128i>(k); }
+        inline __m128d dataD() const { return avx_cast<__m128d>(k); }
+
+        bool operator[](int index) const;
+
+        int count() const;
+        int firstOne() const;
+
+    private:
+        __m128 k;
+};
+
 struct ForeachHelper
 {
-    unsigned long mask;
+    size_t mask;
     bool brk;
-    inline ForeachHelper(unsigned long _mask) : mask(_mask), brk(false) {}
+    inline ForeachHelper(size_t _mask) : mask(_mask), brk(false) {}
     inline bool outer() const { return mask != 0; }
     inline bool inner() { return (brk = !brk); }
-    inline unsigned long next() {
-        const unsigned long bit = __builtin_ctzl(mask);
+    inline size_t next() {
+#if defined(__GNUC__) && !defined(VC_NO_INLINE_ASM)
+        const size_t bit = __builtin_ctzl(mask);
         __asm__("btr %1,%0" : "+r"(mask) : "r"(bit));
+#else
+        const size_t bit = _bit_scan_forward(mask);
+        mask &= ~(1 << bit);
+#endif
         return bit;
     }
 };
@@ -164,138 +195,34 @@ struct ForeachHelper
 
 #define foreach_bit(_it_, _mask_) Vc_foreach_bit(_it_, _mask_)
 
-template<unsigned int Size> inline int Mask<Size>::shiftMask() const
+template<unsigned int Size> inline int Mask<Size, 32u>::shiftMask() const
 {
     return _mm256_movemask_epi8(dataI());
 }
-
-template<> template<> inline Mask<2>::Mask(const Mask<4> &x) {
-    k = _mm256_unpacklo_ps(x.data(), x.data());
-}
-template<> template<> inline Mask<2>::Mask(const Mask<8> &x) {
-    _M256I tmp = _mm256_unpacklo_epi16(x.dataI(), x.dataI());
-    k = _mm256_castsi256_ps(_mm256_unpacklo_epi32(tmp, tmp));
-}
-template<> template<> inline Mask<2>::Mask(const Mask<16> &x) {
-    _M256I tmp = _mm256_unpacklo_epi8(x.dataI(), x.dataI());
-    tmp = _mm256_unpacklo_epi16(tmp, tmp);
-    k = _mm256_castsi256_ps(_mm256_unpacklo_epi32(tmp, tmp));
-}
-template<> template<> inline Mask<4>::Mask(const Mask<2> &x) {
-    k = _mm256_castsi256_ps(_mm256_packs_epi16(x.dataI(), _mm256_setzero_si256()));
-}
-template<> template<> inline Mask<4>::Mask(const Mask<8> &x) {
-    k = _mm256_castsi256_ps(_mm256_unpacklo_epi16(x.dataI(), x.dataI()));
-}
-template<> template<> inline Mask<4>::Mask(const Mask<16> &x) {
-    _M256I tmp = _mm256_unpacklo_epi8(x.dataI(), x.dataI());
-    k = _mm256_castsi256_ps(_mm256_unpacklo_epi16(tmp, tmp));
-}
-template<> template<> inline Mask<8>::Mask(const Mask<2> &x) {
-    _M256I tmp = _mm256_packs_epi16(x.dataI(), x.dataI());
-    k = _mm256_castsi256_ps(_mm256_packs_epi16(tmp, tmp));
-}
-template<> template<> inline Mask<8>::Mask(const Mask<4> &x) {
-    k = _mm256_castsi256_ps(_mm256_packs_epi16(x.dataI(), x.dataI()));
-}
-template<> template<> inline Mask<8>::Mask(const Mask<16> &x) {
-    k = _mm256_castsi256_ps(_mm256_unpacklo_epi8(x.dataI(), x.dataI()));
+template<unsigned int Size> inline int Mask<Size, 16u>::shiftMask() const
+{
+    return _mm_movemask_epi8(dataI());
 }
 
-template<> inline void Mask< 4>::expand(Mask<2> *x) const {
-    x[0].k = _mm256_unpacklo_ps(data(), data());
-    x[1].k = _mm256_unpackhi_ps(data(), data());
-}
-template<> inline void Mask< 8>::expand(Mask<4> *x) const {
-    x[0].k = _mm256_castsi256_ps(_mm256_unpacklo_epi16(dataI(), dataI()));
-    x[1].k = _mm256_castsi256_ps(_mm256_unpackhi_epi16(dataI(), dataI()));
-}
-template<> inline void Mask<16>::expand(Mask<8> *x) const {
-    x[0].k = _mm256_castsi256_ps(_mm256_unpacklo_epi8 (dataI(), dataI()));
-    x[1].k = _mm256_castsi256_ps(_mm256_unpackhi_epi8 (dataI(), dataI()));
-}
-
-template<> inline int Mask< 2>::toInt() const { return _mm256_movemask_pd(dataD()); }
-template<> inline int Mask< 4>::toInt() const { return _mm256_movemask_ps(data ()); }
-template<> inline int Mask< 8>::toInt() const { return _mm256_movemask_epi8(_mm256_packs_epi16(dataI(), _mm256_setzero_si256())); }
-template<> inline int Mask<16>::toInt() const { return _mm256_movemask_epi8(dataI()); }
+template<> inline int Mask< 4, 32>::toInt() const { return _mm256_movemask_pd(dataD()); }
+template<> inline int Mask< 8, 32>::toInt() const { return _mm256_movemask_ps(data ()); }
+template<> inline int Mask< 8, 16>::toInt() const { return _mm_movemask_epi8(_mm_packs_epi16(dataI(), _mm_setzero_si256())); }
+template<> inline int Mask<16, 16>::toInt() const { return _mm_movemask_epi8(dataI()); }
 
 template<> inline bool Mask< 2>::operator[](int index) const { return toInt() & (1 << index); }
 template<> inline bool Mask< 4>::operator[](int index) const { return toInt() & (1 << index); }
 template<> inline bool Mask< 8>::operator[](int index) const { return shiftMask() & (1 << 2 * index); }
 template<> inline bool Mask<16>::operator[](int index) const { return toInt() & (1 << index); }
 
-template<> inline int Mask<2>::count() const
+template<unsigned int Size, size_t Width> inline int Mask<Size, Width>::count() const
 {
-    int mask = _mm256_movemask_pd(dataD());
-    return (mask & 1) + (mask >> 1);
+    return _mm_popcnt_u32(toInt());
 }
 
-template<> inline int Mask<4>::count() const
+template<unsigned int Size, size_t Width> inline int Mask<Size, Width>::firstOne() const
 {
-//X     int tmp = _mm256_movemask_ps(data());
-//X     tmp = (tmp & 5) + ((tmp >> 1) & 5);
-//X     return (tmp & 3) + ((tmp >> 2) & 3);
-    _M256I x = _mm256_srli_epi32(dataI(), 31);
-    x = _mm256_add_epi32(x, _mm256_shuffle_epi32(x, _MM_SHUFFLE(0, 1, 2, 3)));
-    x = _mm256_add_epi32(x, _mm256_shufflelo_epi16(x, _MM_SHUFFLE(1, 0, 3, 2)));
-    return _mm256_cvtsi256_si32(x);
+    return _bit_scan_forward(toInt());
 }
-
-template<> inline int Mask<8>::count() const
-{
-//X     int tmp = _mm256_movemask_epi8(dataI());
-//X     tmp = (tmp & 0x1111) + ((tmp >> 2) & 0x1111);
-//X     tmp = (tmp & 0x0303) + ((tmp >> 4) & 0x0303);
-//X     return (tmp & 0x000f) + ((tmp >> 8) & 0x000f);
-    _M256I x = _mm256_srli_epi16(dataI(), 15);
-    x = _mm256_add_epi16(x, _mm256_shuffle_epi32(x, _MM_SHUFFLE(0, 1, 2, 3)));
-    x = _mm256_add_epi16(x, _mm256_shufflelo_epi16(x, _MM_SHUFFLE(0, 1, 2, 3)));
-    x = _mm256_add_epi16(x, _mm256_shufflelo_epi16(x, _MM_SHUFFLE(2, 3, 0, 1)));
-    return _mm256_extract_epi16(x, 0);
-}
-
-template<> inline int Mask<16>::count() const
-{
-    int tmp = _mm256_movemask_epi8(dataI());
-    tmp = (tmp & 0x5555) + ((tmp >> 1) & 0x5555);
-    tmp = (tmp & 0x3333) + ((tmp >> 2) & 0x3333);
-    tmp = (tmp & 0x0f0f) + ((tmp >> 4) & 0x0f0f);
-    return (tmp & 0x00ff) + ((tmp >> 8) & 0x00ff);
-}
-
-template<unsigned int Size> inline int Mask<Size>::firstOne() const
-{
-    const int mask = toInt();
-    int bit;
-    __asm__("bsf %1,%0" : "=&r"(bit) : "r"(mask));
-    return bit;
-}
-
-/**
- * Loop over all set bits in the mask. The iterator variable will be set to the position of the set
- * bits. A mask of e.g. 00011010 would result in the loop being called with the iterator being set to
- * 1, 3, and 4.
- *
- * This allows you to write:
- * \code
- * float_v a = ...;
- * foreach_bit(int i, a < 0.f) {
- *   std::cout << a[i] << "\n";
- * }
- * \endcode
- * The example prints all the values in \p a that are negative, and only those.
- *
- * \param it   The iterator variable. For example "int i".
- * \param mask The mask to iterate over. You can also just write a vector operation that returns a
- *             mask.
- */
-//X #define foreach_bit(it, mask)
-//X     for (int _avx_vector_foreach_inner = 1, ForeachScope _avx_vector_foreach_scope(mask.toInt()), int it = _avx_vector_foreach_scope.bit(); _avx_vector_foreach_inner; --_avx_vector_foreach_inner)
-//X     for (int _avx_vector_foreach_mask = (mask).toInt(), int _avx_vector_foreach_it = _avx_bitscan(mask.toInt());
-//X             _avx_vector_foreach_it > 0;
-//X             _avx_vector_foreach_it = _avx_bitscan_initialized(_avx_vector_foreach_it, mask.data()))
-//X         for (int _avx_vector_foreach_inner = 1, it = _avx_vector_foreach_it; _avx_vector_foreach_inner; --_avx_vector_foreach_inner)
 
 } // namespace AVX
 } // namespace Vc
