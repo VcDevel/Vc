8136b79be5779ca853a9191c42e7a738621e7e27
Matthias Kretz <kretz@kde.org>
2011-04-20 01:17:56 +0200

    ignore more "wanted" warnings

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -1,7 +1,10 @@
-set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION} "used uninitialized in this function")
-set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION} "warning is a GCC extension")
-set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION} "^-- ") # Ignore output from cmake
-set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION} "^\\*\\*\\* WARNING non-zero return value in ctest from: make") # Ignore output from ctest
+set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION}
+   "used uninitialized in this function"
+   "GCC < 4.3 does not have full support for SSE2 intrinsics." # Ignore self-made warning, though what I really want is a message when the warning is absent
+   "call to 'Vc::Warnings::_operator_bracket_warning' declared with attribute warning"
+   "warning is a GCC extension"
+   "^-- "  # Ignore output from cmake
+   "^\\*\\*\\* WARNING non-zero return value in ctest from: make"  # Ignore output from ctest
 
 set(CTEST_CUSTOM_ERROR_EXCEPTION ${CTEST_CUSTOM_ERROR_EXCEPTION} "^make\\[[1-9]\\]: ")
 set(CTEST_CUSTOM_ERROR_EXCEPTION ${CTEST_CUSTOM_ERROR_EXCEPTION} "^collect2: ld returned . exit status")
