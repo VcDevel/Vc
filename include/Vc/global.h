ea3204567bff549e087eb08b92a8a21f94e15786
Matthias Kretz <kretz@kde.org>
2012-02-06 13:57:56 +0100

    GCC < 4.6 incorrectly uses vmovq instead of movq, so skip this optimization
    fixes 255

--- include/Vc/global.h
+++ include/Vc/global.h
@@ -60,6 +60,7 @@
 
 #  if defined(__AVX__)
 #    define VC_IMPL_AVX 1
+#    define VC_USE_VEX_CODING
 #  else
 #    if defined(__SSE4a__)
 #      define VC_IMPL_SSE 1
@@ -97,6 +98,7 @@
 
 #  if VC_IMPL == AVX // AVX supersedes SSE
 #    define VC_IMPL_AVX 1
+#    define VC_USE_VEX_CODING
 #  elif VC_IMPL == Scalar
 #    define VC_IMPL_Scalar 1
 #  elif VC_IMPL == SSE4a
@@ -112,6 +114,7 @@
 #    endif
 #    ifdef __AVX__
 #      undef __AVX__
+#      define VC_USE_VEX_CODING
 #    endif
 #  elif VC_IMPL == SSE4_2
 #    define VC_IMPL_SSE4_2 1
@@ -125,6 +128,7 @@
 #    endif
 #    ifdef __AVX__
 #      undef __AVX__
+#      define VC_USE_VEX_CODING
 #    endif
 #  elif VC_IMPL == SSE4_1
 #    define VC_IMPL_SSE4_1 1
@@ -140,6 +144,7 @@
 #    endif
 #    ifdef __AVX__
 #      undef __AVX__
+#      define VC_USE_VEX_CODING
 #    endif
 #  elif VC_IMPL == SSSE3
 #    define VC_IMPL_SSSE3 1
@@ -157,6 +162,7 @@
 #    endif
 #    ifdef __AVX__
 #      undef __AVX__
+#      define VC_USE_VEX_CODING
 #    endif
 #  elif VC_IMPL == SSE3
 #    define VC_IMPL_SSE3 1
@@ -176,6 +182,7 @@
 #    endif
 #    ifdef __AVX__
 #      undef __AVX__
+#      define VC_USE_VEX_CODING
 #    endif
 #  elif VC_IMPL == SSE2
 #    define VC_IMPL_SSE2 1
@@ -197,6 +204,7 @@
 #    endif
 #    ifdef __AVX__
 #      undef __AVX__
+#      define VC_USE_VEX_CODING
 #    endif
 #  elif VC_IMPL == SSE
 #    define VC_IMPL_SSE 1
@@ -220,6 +228,7 @@
 #    endif
 #    ifdef __AVX__
 #      undef __AVX__
+#      define VC_USE_VEX_CODING
 #    endif
 #  endif
 #  undef VC_IMPL
