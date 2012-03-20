bf3a473cb19188c304087873988754314cc2dc05
Matthias Kretz <kretz@kde.org>
2012-03-20 17:00:20 +0100

    fix project name

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,9 +1,9 @@
-set(CTEST_PROJECT_NAME "Vc-06")
+set(CTEST_PROJECT_NAME "Vc-0.6")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 CET")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "code.compeng.uni-frankfurt.de")
-set(CTEST_DROP_LOCATION "/dashboard/submit.php?project=Vc-06")
+set(CTEST_DROP_LOCATION "/dashboard/submit.php?project=Vc-0.6")
 
 set(CTEST_DROP_SITE_CDASH TRUE)
 
