2b360e68dea8f7ef5454143a9cd259a714601c15
Matthias Kretz <kretz@kde.org>
2012-04-19 16:33:38 +0200

    port shell code to cmake

--- Test_vc.sh
+++ Test_vc.sh
@@ -1,103 +1,16 @@
 #!/bin/bash
 
-
-function print_example(){
-echo "##################################################################"
-echo "# To set the required parameters for the source and the build    #"
-echo "# directory for ctest, put the export commands below to a        #"
-echo "# separate file which is read during execution and which is      #"
-echo "# defined on the command line.                                   #"
-echo "# Set all parameters according to your needs.                    #"
-echo "# LINUX_FLAVOUR should be set to the distribution you are using  #"
-echo "# eg Debian, SuSe etc.                                           #"
-echo "# This information is only needed to get some information about  #"
-echo "# systems which show problems                                    #"
-echo "# For example                                                    #"
-echo "#!/bin/bash                                                      #"
-echo "#export LINUX_FLAVOUR=Etch32                                     #"
-echo "#export VC_BUILDDIR=<path_to_your_build_directory>               #"
-echo "#export VC_SOURCEDIR=<path_to_source_directory_of_vc>            #"
-echo "##################################################################"
-}
-
-if [ "$#" -lt "1" ]; then
-  echo "Usage: $0 <Experimental|Nightly|Continuous> [config]"
-  echo ""
-  echo "The first parameter is the ctest model:"
-  echo "Possible arguments are Nightly, Continuous, or Experimental."
-  echo ""
-  echo "The second parameter is optional. You may specify a file"
-  echo "containg the information about the setup at the client"
-  echo "installation (see example below)."
-  echo ""
-  print_example
-  exit 1
-fi
-
-# test if a ctest model is either Experimantal or Nightly
-if [ "$1" == "Experimental" -o "$1" == "Nightly" -o "$1" == "Continuous" ]; then
-  echo ""
-else
-  echo "-- Error -- The CTest model \"$1\" is not supported."
-  echo "-- Error -- Possible arguments are Nightly, Continuous, or Experimental."
-  exit 1
-fi  
-
-# test if the input file exists and execute it
-test -f "$2" && source "$2"
-
-test -z "$VC_SOURCEDIR" && VC_SOURCEDIR="`dirname $0`"
-cd "$VC_SOURCEDIR"
-export VC_SOURCEDIR="$PWD" # making sure VC_SOURCEDIR is an absolute path
-
-# set the ctest model to command line parameter
-export ctest_model=$1
-
-# test for architecture
-arch=$(uname -s | tr '[A-Z]' '[a-z]')
-chip=$(uname -m | tr '[A-Z]' '[a-z]')
-
-# extract information about the system and the machine and set
-# environment variables used by ctest
-if test -z "$CXX" ; then
-  COMPILER="`c++ --version 2>&1|head -n1`"
-else
-  COMPILER="`"$CXX" --version 2>&1|head -n1`"
-fi
-case "$arch" in
-  linux)
-    export number_of_processors=$(cat /proc/cpuinfo | grep processor | wc -l)
-    ;;
-  darwin)
-    export number_of_processors=$(sysctl -n hw.ncpu)
-    ;;
-  mingw*)
-    export number_of_processors=$(reg query HKLM\\HARDWARE\\DESCRIPTION\\System\\CentralProcessor|grep -c CentralProcessor)
-    COMPILER="MSVC `cl 2>&1|awk '/Version/ { print $8 }'`"
-    ;;
+case "$1" in
+   Experimental|Nightly|Continuous)
+      export dashboard_model=$1
+      ;;
+   *)
+      echo "Usage: $0 <model>"
+      echo
+      echo "Possible arguments for model are Nightly, Continuous, or Experimental."
+      echo
+      exit 1
+      ;;
 esac
 
-LABEL1="$arch $chip $COMPILER $CXXFLAGS"
-if test "$arch" = "linux"; then
-  test -z "$LINUX_FLAVOUR" && LINUX_FLAVOUR=`lsb_release -d|cut -f2`
-  if test -n "$LINUX_FLAVOUR"; then
-    LABEL1="$LINUX_FLAVOUR $chip $COMPILER $CXXFLAGS"
-  fi
-fi
-export LABEL=$(echo $LABEL1 | tr '[/+]' '[_x]')
-
-export SITE=$(hostname -s 2>/dev/null || hostname)
-export git_branch=`cat .git/HEAD|cut -d/ -f3`
-
-echo "************************"
-date
-echo "LABEL: $LABEL"
-echo "SITE:  $SITE"
-echo "Model: ${ctest_model}"
-echo "Nr. of processes: " $number_of_processors
-echo "************************"
-
-test -z "$VC_BUILDDIR" && export VC_BUILDDIR="$VC_SOURCEDIR/build-${ctest_model}-${LABEL//[\[\] ()]/_}"
-test -d "$VC_BUILDDIR" || mkdir -p "$VC_BUILDDIR"
-
-ctest --interactive-debug-mode 0 -S test.cmake 2>&1 | grep -v 'Error in read script:'
+ctest -S "`dirname $0`/test.cmake" 2>&1 | grep -v 'Error in read script:'
--- test.cmake
+++ test.cmake
@@ -1,14 +1,66 @@
-Set(CTEST_SOURCE_DIRECTORY $ENV{VC_SOURCEDIR})
-Set(CTEST_BINARY_DIRECTORY $ENV{VC_BUILDDIR})
-Set(CTEST_SITE $ENV{SITE})
-Set(CTEST_BUILD_NAME $ENV{LABEL})
+if(NOT CTEST_SOURCE_DIRECTORY)
+   get_filename_component(CTEST_SOURCE_DIRECTORY "${CMAKE_CURRENT_LIST_FILE}" PATH)
+endif()
+
+set(dashboard_model "$ENV{dashboard_model}")
+
+execute_process(COMMAND uname -s OUTPUT_VARIABLE arch OUTPUT_STRIP_TRAILING_WHITESPACE)
+string(TOLOWER "${arch}" arch)
+execute_process(COMMAND uname -m OUTPUT_VARIABLE chip OUTPUT_STRIP_TRAILING_WHITESPACE)
+string(TOLOWER "${chip}" chip)
+if(arch MATCHES "mingw")
+   execute_process(COMMAND cl COMMAND awk "/Version/ { print $8 }" OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE COMPILER_VERSION)
+   set(COMPILER_VERSION "MSVC ${COMPILER_VERSION}")
+   execute_process(COMMAND reg query "HKLM\\HARDWARE\\DESCRIPTION\\System\\CentralProcessor" COMMAND grep -c CentralProcessor OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE number_of_processors)
+else()
+   set(_cxx "$ENV{CXX}")
+   if(NOT _cxx)
+      set(_cxx "c++")
+   endif()
+   execute_process(COMMAND ${_cxx} --version COMMAND head -n1 OUTPUT_VARIABLE COMPILER_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
+   if(arch STREQUAL "darwin")
+      execute_process(COMMAND sysctl -n hw.ncpu OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE number_of_processors)
+   else()
+      execute_process(COMMAND grep -c processor /proc/cpuinfo OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE number_of_processors)
+   endif()
+endif()
+
+if(arch STREQUAL "linux")
+   execute_process(COMMAND lsb_release -d COMMAND cut -f2 OUTPUT_STRIP_TRAILING_WHITESPACE OUTPUT_VARIABLE arch)
+endif()
+set(CTEST_BUILD_NAME "${arch} ${chip} ${COMPILER_VERSION} $ENV{CXXFLAGS}")
+string(REPLACE "/" "_" CTEST_BUILD_NAME "${CTEST_BUILD_NAME}")
+string(REPLACE "+" "x" CTEST_BUILD_NAME "${CTEST_BUILD_NAME}") # CDash fails to escape '+' correctly in URIs
+string(REGEX REPLACE "[][ ()]" "_" CTEST_BINARY_DIRECTORY "${CTEST_BUILD_NAME}")
+set(CTEST_BINARY_DIRECTORY "${CTEST_SOURCE_DIRECTORY}/build-${dashboard_model}-${CTEST_BINARY_DIRECTORY}")
+file(MAKE_DIRECTORY "${CTEST_BINARY_DIRECTORY}")
+
+execute_process(COMMAND hostname -s RESULT_VARIABLE ok OUTPUT_VARIABLE CTEST_SITE ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+if(NOT ok EQUAL 0)
+   execute_process(COMMAND hostname OUTPUT_VARIABLE CTEST_SITE ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+endif()
+
 Set(CTEST_START_WITH_EMPTY_BINARY_DIRECTORY_ONCE TRUE)
-set(CTEST_NOTES_FILES ${CTEST_SOURCE_DIRECTORY}/.git/HEAD ${CTEST_SOURCE_DIRECTORY}/.git/refs/heads/$ENV{git_branch})
+
+file(READ "${CTEST_SOURCE_DIRECTORY}/.git/HEAD" git_branch)
+string(STRIP "${git_branch}" git_branch)
+# -> ref: refs/heads/master
+string(REGEX REPLACE "^.*/" "" git_branch "${git_branch}")
+# -> master
+set(CTEST_NOTES_FILES "${CTEST_SOURCE_DIRECTORY}/.git/HEAD" "${CTEST_SOURCE_DIRECTORY}/.git/refs/heads/${git_branch}")
 
 include(CTestCustom.cmake)
 include(CTestConfig.cmake)
 set(CTEST_USE_LAUNCHERS 1) # much improved error/warning message logging
-set(MAKE_ARGS "-j$ENV{number_of_processors} -i")
+set(MAKE_ARGS "-j${number_of_processors} -i")
+
+message("********************************")
+#message("src:        ${CTEST_SOURCE_DIRECTORY}")
+#message("obj:        ${CTEST_BINARY_DIRECTORY}")
+message("build name: ${CTEST_BUILD_NAME}")
+message("site:       ${CTEST_SITE}")
+message("model:      ${dashboard_model}")
+message("********************************")
 
 if(WIN32)
    find_program(JOM jom)
@@ -26,15 +78,15 @@ else()
 endif()
 
 macro(go)
-   CTEST_START ($ENV{ctest_model})
+   CTEST_START (${dashboard_model})
    set(res 0)
-   if(NOT $ENV{ctest_model} STREQUAL "Experimental")
+   if(NOT ${dashboard_model} STREQUAL "Experimental")
       CTEST_UPDATE (SOURCE "${CTEST_SOURCE_DIRECTORY}" RETURN_VALUE res)
       if(res GREATER 0)
          ctest_submit(PARTS Update)
       endif()
    endif()
-   if(NOT $ENV{ctest_model} STREQUAL "Continuous" OR res GREATER 0)
+   if(NOT ${dashboard_model} STREQUAL "Continuous" OR res GREATER 0)
       CTEST_CONFIGURE (BUILD "${CTEST_BINARY_DIRECTORY}"
          OPTIONS "-DCTEST_USE_LAUNCHERS=${CTEST_USE_LAUNCHERS};-DCMAKE_BUILD_TYPE=Release;-DBUILD_EXAMPLES=TRUE"
          APPEND
@@ -56,7 +108,7 @@ macro(go)
                   BUILD "${CTEST_BINARY_DIRECTORY}"
                   APPEND
                   RETURN_VALUE res
-                  PARALLEL_LEVEL $ENV{number_of_processors}
+                  PARALLEL_LEVEL ${number_of_processors}
                   INCLUDE_LABEL "${subproject}")
                ctest_submit(PARTS Test)
             endif()
@@ -65,7 +117,7 @@ macro(go)
    endif()
 endmacro()
 
-if($ENV{ctest_model} STREQUAL "Continuous")
+if(${dashboard_model} STREQUAL "Continuous")
    while(${CTEST_ELAPSED_TIME} LESS 64800)
       set(START_TIME ${CTEST_ELAPSED_TIME})
       go()
