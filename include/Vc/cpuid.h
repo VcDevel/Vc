9bcdeef4ae9b7201b0ab43a1fda95e88f3199681
Matthias Kretz <kretz@kde.org>
2012-04-23 18:03:37 +0200

    force conversion to bool
    patch from Werner Oestringer <Werner.Oestringer@gmx.de>, thanks!

--- include/Vc/cpuid.h
+++ include/Vc/cpuid.h
@@ -46,40 +46,40 @@ class CpuId
         static inline bool isAmd   () { return s_ecx0 == 0x444D4163; }
         static inline bool isIntel () { return s_ecx0 == 0x6C65746E; }
         static inline bool hasSse3 () { return s_processorFeaturesC & (1 << 0); }
-        static inline bool hasVmx  () { return s_processorFeaturesC & (1 << 5); }
-        static inline bool hasSmx  () { return s_processorFeaturesC & (1 << 6); }
-        static inline bool hasEst  () { return s_processorFeaturesC & (1 << 7); }
-        static inline bool hasTm2  () { return s_processorFeaturesC & (1 << 8); }
-        static inline bool hasSsse3() { return s_processorFeaturesC & (1 << 9); }
-        static inline bool hasPdcm () { return s_processorFeaturesC & (1 << 15); }
-        static inline bool hasSse41() { return s_processorFeaturesC & (1 << 19); }
-        static inline bool hasSse42() { return s_processorFeaturesC & (1 << 20); }
-        static inline bool hasAes  () { return s_processorFeaturesC & (1 << 25); }
-        static inline bool hasOsxsave() { return s_processorFeaturesC & (1 << 27); }
-        static inline bool hasAvx  () { return s_processorFeaturesC & (1 << 28); }
-        static inline bool hasFpu  () { return s_processorFeaturesD & (1 << 0); }
-        static inline bool hasVme  () { return s_processorFeaturesD & (1 << 1); }
-        static inline bool hasDe   () { return s_processorFeaturesD & (1 << 2); }
-        static inline bool hasPse  () { return s_processorFeaturesD & (1 << 3); }
-        static inline bool hasTsc  () { return s_processorFeaturesD & (1 << 4); }
-        static inline bool hasMsr  () { return s_processorFeaturesD & (1 << 5); }
-        static inline bool hasPae  () { return s_processorFeaturesD & (1 << 6); }
-        static inline bool hasMtrr () { return s_processorFeaturesD & (1 << 12); }
-        static inline bool hasCmov () { return s_processorFeaturesD & (1 << 15); }
-        static inline bool hasClfsh() { return s_processorFeaturesD & (1 << 19); }
-        static inline bool hasAcpi () { return s_processorFeaturesD & (1 << 22); }
-        static inline bool hasMmx  () { return s_processorFeaturesD & (1 << 23); }
-        static inline bool hasSse  () { return s_processorFeaturesD & (1 << 25); }
-        static inline bool hasSse2 () { return s_processorFeaturesD & (1 << 26); }
-        static inline bool hasHtt  () { return s_processorFeaturesD & (1 << 28); }
-        static inline bool hasSse4a() { return s_processorFeatures8C & (1 << 6); }
-        static inline bool hasMisAlignSse() { return s_processorFeatures8C & (1 << 7); }
-        static inline bool hasAmdPrefetch() { return s_processorFeatures8C & (1 << 8); }
-        static inline bool hasXop ()        { return s_processorFeatures8C & (1 << 11); }
-        static inline bool hasFma4 ()       { return s_processorFeatures8C & (1 << 16); }
-        static inline bool hasRdtscp()      { return s_processorFeatures8D & (1 << 27); }
-        static inline bool has3DNow()       { return s_processorFeatures8D & (1u << 31); }
-        static inline bool has3DNowExt()    { return s_processorFeatures8D & (1 << 30); }
+        static inline bool hasVmx  () { return (s_processorFeaturesC & (1 << 5)) != 0; }
+        static inline bool hasSmx  () { return (s_processorFeaturesC & (1 << 6)) != 0; }
+        static inline bool hasEst  () { return (s_processorFeaturesC & (1 << 7)) != 0; }
+        static inline bool hasTm2  () { return (s_processorFeaturesC & (1 << 8)) != 0; }
+        static inline bool hasSsse3() { return (s_processorFeaturesC & (1 << 9)) != 0; }
+        static inline bool hasPdcm () { return (s_processorFeaturesC & (1 << 15)) != 0; }
+        static inline bool hasSse41() { return (s_processorFeaturesC & (1 << 19)) != 0; }
+        static inline bool hasSse42() { return (s_processorFeaturesC & (1 << 20)) != 0; }
+        static inline bool hasAes  () { return (s_processorFeaturesC & (1 << 25)) != 0; }
+        static inline bool hasOsxsave() { return (s_processorFeaturesC & (1 << 27)) != 0; }
+        static inline bool hasAvx  () { return (s_processorFeaturesC & (1 << 28)) != 0; }
+        static inline bool hasFpu  () { return (s_processorFeaturesD & (1 << 0)) != 0; }
+        static inline bool hasVme  () { return (s_processorFeaturesD & (1 << 1)) != 0; }
+        static inline bool hasDe   () { return (s_processorFeaturesD & (1 << 2)) != 0; }
+        static inline bool hasPse  () { return (s_processorFeaturesD & (1 << 3)) != 0; }
+        static inline bool hasTsc  () { return (s_processorFeaturesD & (1 << 4)) != 0; }
+        static inline bool hasMsr  () { return (s_processorFeaturesD & (1 << 5)) != 0; }
+        static inline bool hasPae  () { return (s_processorFeaturesD & (1 << 6)) != 0; }
+        static inline bool hasMtrr () { return (s_processorFeaturesD & (1 << 12)) != 0; }
+        static inline bool hasCmov () { return (s_processorFeaturesD & (1 << 15)) != 0; }
+        static inline bool hasClfsh() { return (s_processorFeaturesD & (1 << 19)) != 0; }
+        static inline bool hasAcpi () { return (s_processorFeaturesD & (1 << 22)) != 0; }
+        static inline bool hasMmx  () { return (s_processorFeaturesD & (1 << 23)) != 0; }
+        static inline bool hasSse  () { return (s_processorFeaturesD & (1 << 25)) != 0; }
+        static inline bool hasSse2 () { return (s_processorFeaturesD & (1 << 26)) != 0; }
+        static inline bool hasHtt  () { return (s_processorFeaturesD & (1 << 28)) != 0; }
+        static inline bool hasSse4a() { return (s_processorFeatures8C & (1 << 6)) != 0; }
+        static inline bool hasMisAlignSse() { return (s_processorFeatures8C & (1 << 7)) != 0; }
+        static inline bool hasAmdPrefetch() { return (s_processorFeatures8C & (1 << 8)) != 0; }
+        static inline bool hasXop ()        { return (s_processorFeatures8C & (1 << 11)) != 0; }
+        static inline bool hasFma4 ()       { return (s_processorFeatures8C & (1 << 16)) != 0; }
+        static inline bool hasRdtscp()      { return (s_processorFeatures8D & (1 << 27)) != 0; }
+        static inline bool has3DNow()       { return (s_processorFeatures8D & (1u << 31)) != 0; }
+        static inline bool has3DNowExt()    { return (s_processorFeatures8D & (1 << 30)) != 0; }
         static inline uint   L1Instruction() { return s_L1Instruction; }
         static inline uint   L1Data() { return s_L1Data; }
         static inline uint   L2Data() { return s_L2Data; }
