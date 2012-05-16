7099bbcabb151c2209d04907ed10f06b3a4c960f
Matthias Kretz <kretz@kde.org>
2012-05-16 14:33:35 +0200

    use C++11s static_assert on MSVC 2010

--- common/macros.h
+++ common/macros.h
@@ -156,7 +156,7 @@ do {} while ( false )
 #define _VC_CAT_HELPER(a, b, c, d) a##b##c##d
 #define _VC_CAT(a, b, c, d) _VC_CAT_HELPER(a, b, c, d)
 
-#if __cplusplus >= 201103 // C++11
+#if __cplusplus >= 201103 /*C++11*/ || VC_MSVC >= 160000000
 #define VC_STATIC_ASSERT_NC(cond, msg) \
     static_assert(cond, #msg)
 #define VC_STATIC_ASSERT(cond, msg) VC_STATIC_ASSERT_NC(cond, msg)
