847d2e5258c00264922ef5e79f0753ce0bb964ae
Matthias Kretz <kretz@kde.org>
2012-11-01 15:49:22 +0100

    clang and ICC should be able to show the instruction pointer, too

--- tests/unittest.h
+++ tests/unittest.h
@@ -376,7 +376,7 @@ class _UnitTest_Compare
     private:
         static inline size_t ALWAYS_INLINE getIp() {
             size_t _ip;
-#if defined(__x86_64__) && defined(VC_GCC)
+#if defined(__x86_64__) && defined(VC_GNU_ASM)
             asm("lea 0(%%rip),%0" : "=r"(_ip));
 #else
             _ip = 0;
