360713523a57dbed25f08e9ed4c95e277dd54850
Matthias Kretz <kretz@kde.org>
2013-02-07 11:03:21 +0100

    update tool to create reference data files

--- tests/convert-sincos-reference.cpp
+++ tests/convert-sincos-reference.cpp
@@ -16,7 +16,31 @@ template<typename T> struct Data
     static const Reference<T> asinReference[];
     static const Reference<T> acosReference[];
     static const Reference<T> atanReference[];
+    static const Reference<T> lnReference[];
+    static const Reference<T> log2Reference[];
+    static const Reference<T> log10Reference[];
 };
+
+namespace Function {
+    enum Function {
+        sincos, atan, asin, acos, ln, log2, log10
+    };
+}
+template<typename T, Function::Function F> static inline const char *filenameOut();
+template<> inline const char *filenameOut<float , Function::sincos>() { return "sincos-reference-single.dat"; }
+template<> inline const char *filenameOut<double, Function::sincos>() { return "sincos-reference-double.dat"; }
+template<> inline const char *filenameOut<float , Function::atan  >() { return "atan-reference-single.dat"; }
+template<> inline const char *filenameOut<double, Function::atan  >() { return "atan-reference-double.dat"; }
+template<> inline const char *filenameOut<float , Function::asin  >() { return "asin-reference-single.dat"; }
+template<> inline const char *filenameOut<double, Function::asin  >() { return "asin-reference-double.dat"; }
+template<> inline const char *filenameOut<float , Function::acos  >() { return "acos-reference-single.dat"; }
+template<> inline const char *filenameOut<double, Function::acos  >() { return "acos-reference-double.dat"; }
+template<> inline const char *filenameOut<float , Function::ln  >() { return "reference-ln-sp.dat"; }
+template<> inline const char *filenameOut<double, Function::ln  >() { return "reference-ln-dp.dat"; }
+template<> inline const char *filenameOut<float , Function::log2  >() { return "reference-log2-sp.dat"; }
+template<> inline const char *filenameOut<double, Function::log2  >() { return "reference-log2-dp.dat"; }
+template<> inline const char *filenameOut<float , Function::log10  >() { return "reference-log10-sp.dat"; }
+template<> inline const char *filenameOut<double, Function::log10  >() { return "reference-log10-dp.dat"; }
 
 template<> const SincosReference<float> Data<float>::sincosReference[] = {
 #include "sincos-reference-single.h"
@@ -42,37 +66,55 @@ template<> const Reference<float> Data<float>::atanReference[] = {
 template<> const Reference<double> Data<double>::atanReference[] = {
 #include "atan-reference-double.h"
 };
-
-enum Function {
-    Sincos, Atan, Asin, Acos
+template<> const Reference<float> Data<float>::lnReference[] = {
+#include "reference-ln-sp.h"
 };
-template<typename T, Function F> static inline const char *filenameOut();
-template<> inline const char *filenameOut<float , Sincos>() { return "sincos-reference-single.dat"; }
-template<> inline const char *filenameOut<double, Sincos>() { return "sincos-reference-double.dat"; }
-template<> inline const char *filenameOut<float , Atan  >() { return "atan-reference-single.dat"; }
-template<> inline const char *filenameOut<double, Atan  >() { return "atan-reference-double.dat"; }
-template<> inline const char *filenameOut<float , Asin  >() { return "asin-reference-single.dat"; }
-template<> inline const char *filenameOut<double, Asin  >() { return "asin-reference-double.dat"; }
-template<> inline const char *filenameOut<float , Acos  >() { return "acos-reference-single.dat"; }
-template<> inline const char *filenameOut<double, Acos  >() { return "acos-reference-double.dat"; }
+template<> const Reference<double> Data<double>::lnReference[] = {
+#include "reference-ln-dp.h"
+};
+template<> const Reference<float> Data<float>::log2Reference[] = {
+#include "reference-log2-sp.h"
+};
+template<> const Reference<double> Data<double>::log2Reference[] = {
+#include "reference-log2-dp.h"
+};
+template<> const Reference<float> Data<float>::log10Reference[] = {
+#include "reference-log10-sp.h"
+};
+template<> const Reference<double> Data<double>::log10Reference[] = {
+#include "reference-log10-dp.h"
+};
 
 template<typename T>
 static void convert()
 {
-    FILE *file = fopen(filenameOut<T, Sincos>(), "wb");
+    FILE *file;
+    file = fopen(filenameOut<T, Function::sincos>(), "wb");
     fwrite(&Data<T>::sincosReference[0], sizeof(SincosReference<T>), sizeof(Data<T>::sincosReference) / sizeof(SincosReference<T>), file);
     fclose(file);
 
-    file = fopen(filenameOut<T, Atan>(), "wb");
+    file = fopen(filenameOut<T, Function::atan>(), "wb");
     fwrite(&Data<T>::atanReference[0], sizeof(Reference<T>), sizeof(Data<T>::atanReference) / sizeof(Reference<T>), file);
     fclose(file);
 
-    file = fopen(filenameOut<T, Asin>(), "wb");
+    file = fopen(filenameOut<T, Function::asin>(), "wb");
     fwrite(&Data<T>::asinReference[0], sizeof(Reference<T>), sizeof(Data<T>::asinReference) / sizeof(Reference<T>), file);
     fclose(file);
 
-    file = fopen(filenameOut<T, Acos>(), "wb");
+    file = fopen(filenameOut<T, Function::acos>(), "wb");
     fwrite(&Data<T>::acosReference[0], sizeof(Reference<T>), sizeof(Data<T>::acosReference) / sizeof(Reference<T>), file);
+    fclose(file);
+
+    file = fopen(filenameOut<T, Function::ln>(), "wb");
+    fwrite(&Data<T>::lnReference[0], sizeof(Reference<T>), sizeof(Data<T>::lnReference) / sizeof(Reference<T>), file);
+    fclose(file);
+
+    file = fopen(filenameOut<T, Function::log2>(), "wb");
+    fwrite(&Data<T>::log2Reference[0], sizeof(Reference<T>), sizeof(Data<T>::log2Reference) / sizeof(Reference<T>), file);
+    fclose(file);
+
+    file = fopen(filenameOut<T, Function::log10>(), "wb");
+    fwrite(&Data<T>::log10Reference[0], sizeof(Reference<T>), sizeof(Data<T>::log10Reference) / sizeof(Reference<T>), file);
     fclose(file);
 }
 
