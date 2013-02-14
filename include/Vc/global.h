380c523a9668df01d9ebdca8a932e7075502c385
Matthias Kretz <kretz@kde.org>
2013-02-14 14:07:49 +0100

    "fix" -Wundef warning

--- include/Vc/global.h
+++ include/Vc/global.h
@@ -486,7 +486,7 @@ namespace Internal {
 namespace Warnings
 {
     void _operator_bracket_warning()
-#if VC_HAVE_ATTRIBUTE_WARNING
+#ifdef VC_HAVE_ATTRIBUTE_WARNING
         __attribute__((warning("\n\tUse of Vc::Vector::operator[] to modify scalar entries is known to miscompile with GCC 4.3.x.\n\tPlease upgrade to a more recent GCC or avoid operator[] altogether.\n\t(This warning adds an unnecessary function call to operator[] which should work around the problem at a little extra cost.)")))
 #endif
         ;
