2f2cf6d44a25536399b8cc2085fb1da5a5efb663
Matthias Kretz <kretz@kde.org>
2013-05-02 19:19:44 +0200

    improve iif docs

--- common/iif.h
+++ common/iif.h
@@ -1,6 +1,6 @@
 /*  This file is part of the Vc library. {{{
 
-    Copyright (C) 2012 Matthias Kretz <kretz@kde.org>
+    Copyright (C) 2012-2013 Matthias Kretz <kretz@kde.org>
 
     Vc is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as
@@ -26,7 +26,9 @@
 namespace Vc
 {
 /**
- * Function to mimic the ternary operator '?:'.
+ * \ingroup Utilities
+ *
+ * Function to mimic the ternary operator '?:' (inline-if).
  *
  * \param condition  Determines which values are returned. This is analog to the first argument to
  *                   the ternary operator.
@@ -42,6 +44,9 @@ namespace Vc
  * \code
  * float_v x = Vc::iif (a > 1.f, b, b + c);
  * \endcode
+ *
+ * Assuming \c a has the values [0, 3, 5, 1], \c b is [1, 1, 1, 1], and \c c is [1, 2, 3, 4], then x
+ * will be [2, 2, 3, 5].
  */
 #ifndef VC_MSVC
 template<typename T> static Vc_ALWAYS_INLINE Vector<T> iif (typename Vector<T>::Mask condition, Vector<T> trueValue, Vector<T> falseValue)
@@ -56,17 +61,20 @@ template<typename T> static Vc_ALWAYS_INLINE Vector<T> iif (const typename Vecto
 }
 
 /**
+ * \ingroup Utilities
+ *
  * Overload of the above for boolean conditions.
  *
- * This typically results in direct use of the ternary operator.
+ * This typically results in direct use of the ternary operator. This function makes it easier to
+ * switch from a Vc type to a builtin type.
  *
- * \param condition  Determines which values are returned. This is analog to the first argument to
+ * \param condition  Determines which value is returned. This is analog to the first argument to
  *                   the ternary operator.
- * \param trueValue  The values to return where \p condition is \c true.
- * \param falseValue The values to return where \p condition is \c false.
- * \return A combination of entries from \p trueValue and \p falseValue, according to \p condition.
+ * \param trueValue  The value to return if \p condition is \c true.
+ * \param falseValue The value to return if \p condition is \c false.
+ * \return Either \p trueValue or \p falseValue, depending on \p condition.
  */
-template<typename T> static Vc_ALWAYS_INLINE Vector<T> iif (bool condition, const T &trueValue, const T &falseValue)
+template<typename T> static Vc_ALWAYS_INLINE T iif (bool condition, const T &trueValue, const T &falseValue)
 {
     return condition ? trueValue : falseValue;
 }
