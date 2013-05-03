04a16ed720fad434afc1a2c0846c19851fa87f45
Matthias Kretz <kretz@kde.org>
2013-05-03 15:11:42 +0200

    test compares between mask objects

--- tests/mask.cpp
+++ tests/mask.cpp
@@ -324,11 +324,25 @@ template<typename V> void maskInit()/*{{{*/
     COMPARE(M(Vc::Zero), M(false));
 }
 /*}}}*/
+template<typename V> void maskCompare()/*{{{*/
+{
+    int i = 0;
+    auto m0 = allMasks<V>(i);
+    auto m1 = allMasks<V>(i);
+    while (any_of(m0)) {
+        ++i;
+        VERIFY(m0 == m1);
+        m0 = allMasks<V>(i);
+        VERIFY(m0 != m1);
+        m1 = allMasks<V>(i);
+    }
+}/*}}}*/
 int main(int argc, char **argv)/*{{{*/
 {
     initTest(argc, argv);
 
     testAllTypes(maskInit);
+    testAllTypes(maskCompare);
     testAllTypes(testInc);
     testAllTypes(testDec);
     testAllTypes(testPlusEq);
