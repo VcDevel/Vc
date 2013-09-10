bb2a29f9f47cc4c0251ec3a3e7fe19808ab86d09
Matthias Kretz <kretz@kde.org>
2013-09-10 09:40:06 +0200

    mark SSE::Vector::sorted inline also in the declaration - otherwise GCC 4.5 barfs

--- sse/vector.h
+++ sse/vector.h
@@ -397,7 +397,7 @@ template<typename T> class Vector
 
         Vc_INTRINSIC_L Vector shifted(int amount) const Vc_INTRINSIC_R;
         Vc_INTRINSIC_L Vector rotated(int amount) const Vc_INTRINSIC_R;
-        Vc_ALWAYS_INLINE Vc_PURE Vector sorted() const { return SortHelper<VectorType, Size>::sort(data()); }
+        inline Vc_PURE Vector sorted() const { return SortHelper<VectorType, Size>::sort(data()); }
 
         template<typename F> void callWithValuesSorted(F &f) {
             EntryType value = d.m(0);
