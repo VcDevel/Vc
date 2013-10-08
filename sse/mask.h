682beabe2560558372a94d22380085c5106146b3
Matthias Kretz <kretz@kde.org>
2013-10-08 10:46:51 +0200

    forward the loadstore flag to load

--- sse/mask.h
+++ sse/mask.h
@@ -101,7 +101,7 @@ template<typename T> class Mask
             : d(sse_cast<__m128>(internal::mask_cast<Mask<U>::Size, Size>(rhs.dataI()))) {}
 
         Vc_ALWAYS_INLINE explicit Mask(const bool *mem) { load(mem); }
-        template<typename Flags> Vc_ALWAYS_INLINE explicit Mask(const bool *mem, Flags f) { load(mem); }
+        template<typename Flags> Vc_ALWAYS_INLINE explicit Mask(const bool *mem, Flags f) { load(mem, f); }
 
         Vc_ALWAYS_INLINE_L void load(const bool *mem) Vc_ALWAYS_INLINE_R;
         template<typename Flags> Vc_ALWAYS_INLINE void load(const bool *mem, Flags) { load(mem); }
