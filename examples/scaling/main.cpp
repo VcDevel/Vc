c13da9aa5d32cf38c2f1478056bdde8ca9f4cb29
Matthias Kretz <kretz@kde.org>
2013-11-13 22:33:11 +0100

    document and improve scaling example

--- examples/scaling/main.cpp
+++ examples/scaling/main.cpp
@@ -34,33 +34,46 @@ using Vc::float_v;
  * load/store. Understanding this can help to create better implementations.
  */
 
-template<template<std::size_t N, std::size_t M, int, std::size_t> class Work, std::size_t N = 256, std::size_t M = 4, std::size_t FLOPs = 2> struct Runner
+/*
+ * The Runner is a method to generate the different scenarios with all parameters to the Work
+ * available as constant expressions.
+ * The idea is to have the compiler able to optimize as much as possible so that the actual workload
+ * alone is benchmarked.
+ *
+ * The Runner recursively calls operator() on the Work template class with varying arguments for N
+ * and FLOPs.
+ */
+template<template<std::size_t N, std::size_t M, int, int> class Work, std::size_t N = 256, std::size_t M = 4, int FLOPs = 2> struct Runner
 {
     static void run() {
-        Work<N, M, 4, FLOPs>()();
-        Runner<Work, N, M, FLOPs + 1>::run();
+        Work<N, M, (N > 4096 ? 1 : 4096 / N), FLOPs>()();
+        Runner<Work, N, M, int(FLOPs * 1.5)>::run();
     }
 };
 
-template<template<std::size_t N, std::size_t M, int, std::size_t> class Work, std::size_t N, std::size_t M> struct Runner<Work, N, M, 33>
+template<template<std::size_t N, std::size_t M, int, int> class Work, std::size_t N, std::size_t M> struct Runner<Work, N, M, 211>
 {
     static void run() {
         Runner<Work, N * 2, M>::run();
     }
 };
-template<template<std::size_t N, std::size_t M, int, std::size_t> class Work, std::size_t M, std::size_t FLOPs> struct Runner<Work, 256 * 1024 * 1024, M, FLOPs>
+template<template<std::size_t N, std::size_t M, int, int> class Work, std::size_t M, int FLOPs> struct Runner<Work, 256 * 1024 * 1024, M, FLOPs>
 {
     static void run() {
     }
 };
 
-template<std::size_t FLOPs> struct Flops
+/*
+ * The Flops helper struct generates code that executes FLOPs many floating-point SIMD instructions
+ * (add, sub, and mul)
+ */
+template<int FLOPs> struct Flops
 {
     inline float_v operator()(float_v a, float_v b, float_v c)
     {
-        typedef Flops<(FLOPs - 1) / 2> F1;
-        typedef Flops<FLOPs / 2> F2;
-        return F1()(a, b, c) + F2()(b, c, a);
+        typedef Flops<(FLOPs - 5) / 2> F1;
+        typedef Flops<(FLOPs - 4) / 2> F2;
+        return F1()(a + b, a * b, c) + F2()(a * c, b + c, a);
     }
 };
 
@@ -74,10 +87,29 @@ template<> inline float_v Flops<3>::operator()(float_v a, float_v b, float_v c)
 }
 template<> inline float_v Flops<4>::operator()(float_v a, float_v b, float_v c)
 {
-    return a * b + (b * c - a);
+    return (a * b + c) + a * c;
+}
+template<> inline float_v Flops<5>::operator()(float_v a, float_v b, float_v c)
+{
+    return a * b + (a + c) + a * c;
+}
+template<> inline float_v Flops<6>::operator()(float_v a, float_v b, float_v c)
+{
+    return (a * b + (a + c)) + (a * c - b);
+}
+template<> inline float_v Flops<7>::operator()(float_v a, float_v b, float_v c)
+{
+    return (a * b + (a + c)) + (a * c - (b + c));
+}
+template<> inline float_v Flops<8>::operator()(float_v a, float_v b, float_v c)
+{
+    return (a * b + (a + c) + b) + (a * c - (b + c));
 }
 
-template<std::size_t _N, std::size_t M, int Repetitions, std::size_t FLOPs>
+/*
+ * This is the benchmark code. It is called from Runner and uses Flops to do the work.
+ */
+template<std::size_t _N, std::size_t M, int Repetitions, int FLOPs>
 struct ScaleWorkingSetSize
 {
     void operator()()
@@ -95,6 +127,7 @@ struct ScaleWorkingSetSize
         double throughput = 0.;
         for (std::size_t i = 0; i < 2 + 512 / N; ++i) {
             tsc.start();
+            // ------------- start of the benchmarked code ---------------
             for (int repetitions = 0; repetitions < Repetitions; ++repetitions) {
                 for (std::size_t m = 0; m < M; ++m) {
                     for (std::size_t n = 0; n < N; ++n) {
@@ -104,13 +137,14 @@ struct ScaleWorkingSetSize
                     }
                 }
             }
+            // -------------- end of the benchmarked code ----------------
             tsc.stop();
 
             throughput = std::max(throughput, (Repetitions * M * N * float_v::Size * FLOPs) / static_cast<double>(tsc.cycles()));
         }
 
         const long bytes = N * M * sizeof(float_v);
-        printf("%lu %f %f\n", bytes, static_cast<double>(float_v::Size * FLOPs) / (4 * sizeof(float_v)), throughput
+        printf("%'10lu Byte | %4.2f FLOP/Byte | %4.1f FLOP/cycle\n", bytes, static_cast<double>(float_v::Size * FLOPs) / (4 * sizeof(float_v)), throughput
                 );
     }
 };
@@ -118,7 +152,7 @@ struct ScaleWorkingSetSize
 int main()
 {
     ScaleWorkingSetSize<256, 4, 10, 2>()();
-    printf("%s\t%s\t%s\n", "Working-Set Size (Bytes)", "FLOPs per Byte", "Throughput (FLOPs/Cycle)");
+    printf("%10s | %4s | %4s\n", "Working-Set Size", "FLOPs per Byte", "Throughput (FLOPs/Cycle)");
     Runner<ScaleWorkingSetSize>::run();
     return 0;
 }
