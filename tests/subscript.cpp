5eb564ccd419c4b787a824061fec419a9695c83a
Matthias Kretz <kretz@kde.org>
2014-01-14 14:08:58 +0100

    test scatters and test initializer_list gathers with simd_array

--- tests/subscript.cpp
+++ tests/subscript.cpp
@@ -39,7 +39,7 @@ TEST_BEGIN(V, init, (ALL_VECTORS))
 }
 TEST_END
 
-TEST_BEGIN(V, gathers, (ALL_VECTORS))
+TEST_BEGIN(V, gathers, (SIMD_ARRAYS(32), ALL_VECTORS))
 {
     typedef typename V::EntryType T;
     typedef typename V::IndexType IT;
@@ -56,6 +56,56 @@ TEST_BEGIN(V, gathers, (ALL_VECTORS))
     COMPARE(test, V::IndexesFromZero());
 
     IT indexes = abs(IT::Random()) % 256;
+    test = data[indexes];
+    COMPARE(test, static_cast<V>(indexes));
+    test = data2[indexes];
+    COMPARE(test, static_cast<V>(indexes));
+}
+TEST_END
+
+TEST_BEGIN(V, scatters, (SIMD_ARRAYS(32), ALL_VECTORS))
+{
+    typedef typename V::EntryType T;
+    typedef typename V::IndexType IT;
+    Vc::array<T, 256> data1;
+    Vc::vector<T> data2(256);
+    std::fill_n(&data1[0], 256, 0);
+    std::fill_n(&data2[0], 256, 0);
+
+    data1[IT::IndexesFromZero()] = V::IndexesFromZero();
+    data2[IT::IndexesFromZero()] = V::IndexesFromZero();
+
+    for (size_t i = 0; i < V::Size; ++i) {
+        COMPARE(data1[i], i);
+        COMPARE(data2[i], i);
+    }
+
+    for (int repetition = 0; repetition < 1024 / V::Size; ++repetition) {
+        IT indexes = abs(IT::Random()) % 256;
+        data1[indexes] = V::IndexesFromZero();
+        data2[indexes] = V::IndexesFromZero();
+
+        for (size_t i = 0; i < V::Size; ++i) {
+            COMPARE(data1[indexes[i]], i);
+            COMPARE(data2[indexes[i]], i);
+        }
+    }
+}
+TEST_END
+
+TEST_BEGIN(V, fixedWidthGathers, (SIMD_ARRAYS(4)))
+{
+    typedef typename V::EntryType T;
+    typedef typename V::IndexType IT;
+    Vc::array<T, 256> data;
+    Vc::vector<T> data2(256);
+    for (int i = 0; i < data.size(); ++i) {
+        data[i] = i;
+        data2[i] = i;
+    }
+    V test;
+
+    const auto indexes = { 0, 5, 8, 3 };
     test = data[indexes];
     COMPARE(test, static_cast<V>(indexes));
     test = data2[indexes];
