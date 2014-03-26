5c25b1030593b1283284bcaee99173897e4c17ba
Matthias Kretz <kretz@kde.org>
2014-03-26 13:38:27 +0100

    warnings: warn about unused functions and typedefs
    There are users out there who want to have these warnings enabled and
    include Vc with -I instead of -isystem. Whatever. I liked to have the
    typedefs around, but it shouldn't be much work to readd them when needed
    again. So I removed all unused typedefs.
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- cmake/VcMacros.cmake
+++ cmake/VcMacros.cmake
@@ -208,7 +208,6 @@ macro(vc_set_preferred_compiler_flags)
          AddCompilerFlag("-Wpointer-arith")
          AddCompilerFlag("-Wcast-align")
          AddCompilerFlag("-Wreturn-type")
-         AddCompilerFlag("-Wno-unused-function")
          AddCompilerFlag("-ansi")
          AddCompilerFlag("-pedantic")
          AddCompilerFlag("-Wno-long-long")
@@ -229,8 +228,8 @@ macro(vc_set_preferred_compiler_flags)
       #                                              GCC                                               #
       ##################################################################################################
       if(_add_warning_flags)
-         set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -W -Wall -Wswitch -Wformat -Wchar-subscripts -Wparentheses -Wmultichar -Wtrigraphs -Wpointer-arith -Wcast-align -Wreturn-type -Wno-unused-function -pedantic -Wno-long-long -Wshadow")
-         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -W -Wall -Wswitch -Wformat -Wchar-subscripts -Wparentheses -Wmultichar -Wtrigraphs -Wpointer-arith -Wcast-align -Wreturn-type -Wno-unused-function -pedantic -Wno-long-long -Wshadow")
+         set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -W -Wall -Wswitch -Wformat -Wchar-subscripts -Wparentheses -Wmultichar -Wtrigraphs -Wpointer-arith -Wcast-align -Wreturn-type -pedantic -Wno-long-long -Wshadow")
+         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -W -Wall -Wswitch -Wformat -Wchar-subscripts -Wparentheses -Wmultichar -Wtrigraphs -Wpointer-arith -Wcast-align -Wreturn-type -pedantic -Wno-long-long -Wshadow")
          if(NOT WIN32)
             # the -ansi flag makes MinGW unusable, so maybe it's better to omit it
             set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -ansi")
@@ -242,11 +241,6 @@ macro(vc_set_preferred_compiler_flags)
          if(Vc_GCC_VERSION VERSION_GREATER "4.5.2" AND Vc_GCC_VERSION VERSION_LESS "4.6.4")
             # GCC gives bogus "array subscript is above array bounds" warnings in math.cpp
             AddCompilerFlag("-Wno-array-bounds")
-         endif()
-         if(Vc_GCC_VERSION VERSION_GREATER "4.7.99")
-            # GCC 4.8 warns about stuff we don't care about
-            # Some older GCC versions have problems to note that they don't support the flag
-            AddCompilerFlag("-Wno-unused-local-typedefs")
          endif()
       endif()
       vc_add_compiler_flag(Vc_DEFINITIONS "-Wabi")
