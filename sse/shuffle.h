ef8e5fc851a8247fbec8e52d7eee7ddb5b0da1a7
Matthias Kretz <kretz@kde.org>
2014-04-08 17:08:15 +0200

    make permute helper accept Const0 args for zeroing
    _mm256_permute2f128_* can zero the reult in addition to permutation. Now
    this is also possible via the Mem::permute helpers.
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- sse/shuffle.h
+++ sse/shuffle.h
@@ -26,7 +26,8 @@ namespace Vc_VERSIONED_NAMESPACE
 {
     enum VecPos {
         X0, X1, X2, X3, X4, X5, X6, X7,
-        Y0, Y1, Y2, Y3, Y4, Y5, Y6, Y7
+        Y0, Y1, Y2, Y3, Y4, Y5, Y6, Y7,
+        Const0
     };
 }
 
