9d73c086cfd414537ddddacc0f9f5c729ebf805f
Matthias Kretz <kretz@kde.org>
2014-08-28 14:17:57 +0200

    simdize: support simdize<...bool...>
    Uses Vc::Mask<T0> or Vc::simd_mask_array<T0, N> when a bool needs to be
    simdized.
    + some documentation of the template parameters to
    make_vector_or_simdarray_impl.
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- examples/kdtree/simdize.h
+++ examples/kdtree/simdize.h
@@ -34,32 +34,62 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 namespace simdize_internal
 {
-template <std::size_t N, typename T, bool = std::is_arithmetic<T>::value>
+template <std::size_t N,  // if non-zero requests that number of entries in "Vector<T>",
+                          // which determines the choice between Vector<T> and
+                          // simdarray<T, N>
+          typename T0,  // This identifies the first type that was transformed and is used
+                        // when bool needs to be converted to a Vc::Mask type, in which
+                        // case the underlying type would be arbitrary. With T0 it can be
+                        // smarter and choose between Mask<T0> and simd_mask_array<T0, N>
+          typename T,   // The type to be converted to a Vector or Mask type.
+          bool = std::is_same<T, bool>::value || std::is_same<T, short>::value ||
+                 std::is_same<T, unsigned short>::value || std::is_same<T, int>::value ||
+                 std::is_same<T, unsigned int>::value || std::is_same<T, float>::value ||
+                 std::is_same<T, double>::value
+                 // Flag to easily distinguish types that
+                 // need more recursion for transformation
+                 // (or no transformation at all
+          >
 struct make_vector_or_simdarray_impl;
 
-template <std::size_t N, typename T> struct make_vector_or_simdarray_impl<N, T, true>
+template <std::size_t N, typename T0, typename T>
+struct make_vector_or_simdarray_impl<N, T0, T, true>
 {
     using type = typename std::conditional<N == Vc::Vector<T>::Size, Vc::Vector<T>,
                                            Vc::simdarray<T, N>>::type;
 };
-template <typename T> struct make_vector_or_simdarray_impl<0, T, true>
+template <std::size_t N, typename T0>
+struct make_vector_or_simdarray_impl<N, T0, bool, true>
+{
+    using type = typename std::conditional<N == Vc::Mask<T0>::Size, Vc::Mask<T0>,
+                                           Vc::simd_mask_array<T0, N>>::type;
+};
+template <typename T0> struct make_vector_or_simdarray_impl<0, T0, bool, true>
+{
+    using type = Vc::Mask<T0>;
+};
+template <> struct make_vector_or_simdarray_impl<0, bool, bool, true>
+{
+    using type = Vc::Mask<float>;
+};
+template <typename T> struct make_vector_or_simdarray_impl<0, T, T, true>
 {
     using type = Vc::Vector<T>;
 };
 
-template <std::size_t N, template <typename...> class C, typename... Ts>
-struct make_vector_or_simdarray_impl<N, C<Ts...>, false>;
+template <std::size_t N, typename T0, template <typename...> class C, typename... Ts>
+struct make_vector_or_simdarray_impl<N, T0, C<Ts...>, false>;
 
-template <template <typename, std::size_t> class C, typename T0, std::size_t N,
-          std::size_t M>
-struct make_vector_or_simdarray_impl<N, C<T0, M>, false>;
+template <typename T0, template <typename, std::size_t> class C, typename T,
+          std::size_t N, std::size_t M>
+struct make_vector_or_simdarray_impl<N, T0, C<T, M>, false>;
 
 /** \internal
  * A SIMD Vector type of \p T, either as Vc::simdarray or Vc::Vector, depending on \p N.
  * If Vector<T> has a size equal to N, Vector<T> is used, otherwise simdarray<T, N>.
  */
-template <std::size_t N, typename T>
-using make_vector_or_simdarray = typename make_vector_or_simdarray_impl<N, T>::type;
+template <std::size_t N, typename T0, typename T = T0>
+using make_vector_or_simdarray = typename make_vector_or_simdarray_impl<N, T0, T>::type;
 
 template <typename T, std::size_t N = 0> struct Adapter;
 
@@ -97,17 +127,22 @@ public:
     void operator delete[](void *, void *) {}
 };
 
+template <std::size_t N, template <typename...> class C, typename T0, typename... Ts>
+using make_adapter_base_type = C<
+    make_vector_or_simdarray<N, T0>,
+    make_vector_or_simdarray<
+        Vc::Traits::simd_vector_size<make_vector_or_simdarray<N, T0>>::value, T0, Ts>...>;
+
 template <template <typename...> class C, typename T0, typename... Ts, std::size_t N>
-class Adapter<C<T0, Ts...>, N>
-    : public C<make_vector_or_simdarray<N, T0>, make_vector_or_simdarray<N, Ts>...>
+class Adapter<C<T0, Ts...>, N> : public make_adapter_base_type<N, C, T0, Ts...>
 {
     using Scalar = C<T0, Ts...>;
-    using Vector = C<make_vector_or_simdarray<N, T0>, make_vector_or_simdarray<N, Ts>...>;
+    using Vector = make_adapter_base_type<N, C, T0, Ts...>;
 
 public:
     using FirstVectorType = make_vector_or_simdarray<N, T0>;
-    using VectorTypesTuple =
-        std::tuple<FirstVectorType, make_vector_or_simdarray<N, Ts>...>;
+    using VectorTypesTuple = std::tuple<
+        FirstVectorType, make_vector_or_simdarray<FirstVectorType::Size, T0, Ts>...>;
 
     static constexpr std::size_t Size = FirstVectorType::Size;
     static constexpr std::size_t size() { return Size; }
@@ -132,17 +167,17 @@ public:
     void operator delete[](void *, void *) {}
 };
 
-template <std::size_t N, template <typename...> class C, typename... Ts>
-struct make_vector_or_simdarray_impl<N, C<Ts...>, false>
+template <std::size_t N, typename T0, template <typename...> class C, typename... Ts>
+struct make_vector_or_simdarray_impl<N, T0, C<Ts...>, false>
 {
     using type = Adapter<C<Ts...>, N>;
 };
 
-template <template <typename, std::size_t> class C, typename T0, std::size_t N,
-          std::size_t M>
-struct make_vector_or_simdarray_impl<N, C<T0, M>, false>
+template <typename T0, template <typename, std::size_t> class C, typename T,
+          std::size_t N, std::size_t M>
+struct make_vector_or_simdarray_impl<N, T0, C<T, M>, false>
 {
-    using type = Adapter<C<T0, M>, N>;
+    using type = Adapter<C<T, M>, N>;
 };
 
 }  // namespace simdize_internal
