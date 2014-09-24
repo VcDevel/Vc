3a1bac386b2f5c7731534bd5663b678c74ce1bfb
Matthias Kretz <kretz@kde.org>
2014-09-24 16:20:32 +0200

    NEON: move VectorTraits specialization to intrinsics.h
    The types.h header may not rely on any ARM platform specific types. It
    did in the form of the intrinsic types, that were declared via the
    VectorTraits helper type.
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- neon/types.h
+++ neon/types.h
@@ -66,6 +66,7 @@ template <typename T> struct is_vector : public std::false_type {};
 template <typename T> struct is_vector<Vector<T>> : public std::true_type {};
 template <typename T> struct is_mask : public std::false_type {};
 template <typename T> struct is_mask<Mask<T>> : public std::true_type {};
+
 //template <typename T> struct VectorTraits;
 }
 
