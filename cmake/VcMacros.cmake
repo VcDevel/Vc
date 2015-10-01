3a57923ceb322a79424f89332743ce54a0dbfd4d
Matthias Kretz <kretz@kde.org>
2015-10-01 11:23:31 +0200

    cmake: GCC 4.8.0 is broken
    C++11's alignas & constexpr does not work with GCC 4.8.0. This is fixed
    in GCC 4.8.1.
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- cmake/VcMacros.cmake
+++ cmake/VcMacros.cmake
@@ -99,8 +99,8 @@ macro(vc_determine_compiler)
          endif()
 
          # break build with too old GCC as early as possible.
-         if(Vc_GCC_VERSION VERSION_LESS 4.8.0)
-            message(FATAL_ERROR "Vc 1.x requires C++11 support. This requires at least GCC 4.8.0")
+         if(Vc_GCC_VERSION VERSION_LESS 4.8.1)
+            message(FATAL_ERROR "Vc 1.x requires C++11 support. This requires at least GCC 4.8.1")
          endif()
       else()
          message(WARNING "Untested/-supported Compiler (${CMAKE_CXX_COMPILER}) for use with Vc.\nPlease fill out the missing parts in the CMake scripts and submit a patch to http://code.compeng.uni-frankfurt.de/projects/vc")
