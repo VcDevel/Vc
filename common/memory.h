cbb1d4db4cf06e0ec430fb6af22d60ab54850373
Matthias Kretz <kretz@kde.org>
2015-10-09 12:55:10 +0200

    Memory: really fix the calculation of PaddedSize
    The calculation of padding isn't really connected to alignment. It's
    about filling up the array to contain an integral number of V objects.
    Fixed for real now.
    
    Fixes gh-43
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- common/memory.h
+++ common/memory.h
@@ -278,11 +278,11 @@ class Memory<V, Size, 0u, InitPadding>
             friend class MemoryBase<V, Memory<V, Size, 0u, InitPadding>, 1, void>;
             friend class MemoryDimensionBase<V, Memory<V, Size, 0u, InitPadding>, 1, void>;
             enum : size_t {
-                Alignment = V::MemoryAlignment,  // in Bytes
-                AlignmentMask = Alignment - 1,
-                MaskedSize = Size * sizeof(EntryType) & AlignmentMask,
-                Padding = Alignment - MaskedSize,
-                PaddedSize = MaskedSize == 0 ? Size : Size + Padding / sizeof(EntryType)
+                Alignment = V::MemoryAlignment,     // in Bytes
+                MaskedSize = Size & (V::Size - 1),  // the fraction of Size that exceeds
+                                                    // an integral multiple of V::Size
+                Padding = V::Size - MaskedSize,
+                PaddedSize = MaskedSize == 0 ? Size : Size + Padding
             };
             alignas(static_cast<size_t>(Alignment))  // GCC complains about 'is not an
                                                      // integer constant' unless the
