23aa76317f6e09668d997d31b62cfb198d21a977
Matthias Kretz <kretz@kde.org>
2015-10-19 21:51:01 +0200

    Test all gathers additionally with SimdArray<int, 7>
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- tests/gather.cpp
+++ tests/gather.cpp
@@ -30,7 +30,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <iostream>
 #include <Vc/array>
 
-#define ALL_TYPES (ALL_VECTORS)
+#define ALL_TYPES (ALL_VECTORS, SimdArray<int, 7>)
 
 using namespace Vc;
 
