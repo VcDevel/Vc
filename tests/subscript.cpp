552e70b5690eaa0ff18ea9d72909a33f48ecd66c
Matthias Kretz <kretz@kde.org>
2015-10-19 19:54:45 +0200

    Ensure the indexes vector is sane for scatters.
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- tests/subscript.cpp
+++ tests/subscript.cpp
@@ -74,6 +74,9 @@ template <typename V, int Modulo> static V randomIndexes()
             indexes[i] = (indexes[i] + 1) % Modulo;
         }
     }
+    VERIFY(none_of(indexes.sorted() == indexes.sorted().rotated(1)))
+        << "\nindexes must not contain duplicate values, otherwise the scatter tests "
+           "will fail without actually doing anything wrong!\nindexes: " << indexes;
     return indexes;
 }
 
