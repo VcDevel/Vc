b41be554cd1c6279b4e95f502ff0cd09500dce83
Matthias Kretz <kretz@kde.org>
2016-02-22 13:09:17 +0100

    cmake: make configure-arm smarter
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- configure-arm.sh
+++ configure-arm.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
 srcdir=`dirname $0`
-CXX=arm-linux-gnueabi-g++ CC=arm-linux-gnueabi-gcc cmake "-DCMAKE_TOOLCHAIN_FILE=$srcdir/cmake/toolchain-arm-linux.cmake" "$srcdir"
+skip=false
+test -n "$CXX" -a -n "$CC" && \
+   case "`$CXX -dumpmachine`" in
+      aarch64-linux-*)
+         skip=true
+         ;;
+      arm-linux-*)
+         skip=true
+         ;;
+   esac
+
+if $skip; then
+   echo "Using $CXX"
+   # nothing
+elif which aarch64-linux-gnueabi-g++ >/dev/null; then
+   export CXX=aarch64-linux-gnueabi-g++ CC=aarch64-linux-gnueabi-gcc
+elif which arm-linux-gnueabihf-g++ >/dev/null; then
+   export CXX=arm-linux-gnueabihf-g++ CC=arm-linux-gnueabihf-gcc
+elif which arm-linux-gnueabi-g++ >/dev/null; then
+   export CXX=arm-linux-gnueabi-g++ CC=arm-linux-gnueabi-gcc
+else
+   echo "No suitable compiler found." 1>&2
+   exit 1
+fi
+cmake "-DCMAKE_TOOLCHAIN_FILE=$srcdir/cmake/toolchain-arm-linux.cmake" "$srcdir"
