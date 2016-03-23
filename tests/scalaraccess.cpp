494a255852e138d351de0f92528f8fe706bfbe8c
Matthias Kretz <kretz@kde.org>
2016-03-23 14:43:35 +0100

    Add test for operators not usable on lvalues
    Refs: gh-115
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- tests/scalaraccess.cpp
+++ tests/scalaraccess.cpp
@@ -125,18 +125,25 @@ TEST_TYPES(V, writes, (ALL_VECTORS, SIMD_ARRAY_LIST))
     });
 }
 
-#define INT_OP(op, name)                                                                  \
-    template <typename V> bool test_##name##_assign(V &, float, int) { return false; }   \
-    template <typename V, typename = decltype(std::declval<V &>()[0] op## =              \
-                                                  typename V::EntryType())>              \
-    bool test_##name##_assign(V &a, int x, int y)                                        \
+template <typename T> using decayed = typename std::decay<T>::type;
+template <typename V> using EntryType = typename decayed<V>::EntryType;
+
+#define INT_OP(op, name)                                                                 \
+    template <typename V> bool test_##name##_assign(V &&, float, int) { return false; }  \
+    template <typename V,                                                                \
+              typename = decltype(std::declval<V>()[0] op## = EntryType<V>())>           \
+    bool test_##name##_assign(V &&a, int x, int y)                                       \
     {                                                                                    \
-        using T = typename V::EntryType;                                                 \
+        using T = EntryType<V>;                                                          \
         COMPARE(a[0] op## = T(x), T(y));                                                 \
         COMPARE(a[0], T(y));                                                             \
         return true;                                                                     \
     }                                                                                    \
     Vc_NOTHING_EXPECTING_SEMICOLON
+INT_OP(+, plus);
+INT_OP(-, minus);
+INT_OP(*, times);
+INT_OP(/, divide);
 INT_OP(%, percent);
 INT_OP(<<, lshift);
 INT_OP(>>, rshift);
@@ -145,22 +152,94 @@ INT_OP(&, band);
 INT_OP(^, bxor);
 #undef INT_OP
 
+template <typename V> bool test_pre_increment(V &&, float) { return false; }
+template <typename V, typename = decltype(++std::declval<V>()[0])>
+bool test_pre_increment(V &&a, int x)
+{
+    using T = EntryType<V>;
+    COMPARE(++a[0], T(x));
+    COMPARE(a[0], T(x));
+    return true;
+}
+template <typename V> bool test_post_increment(V &&, float) { return false; }
+template <typename V, typename = decltype(std::declval<V>()[0]++)>
+bool test_post_increment(V &&a, int x)
+{
+    using T = EntryType<V>;
+    COMPARE(a[0]++, T(x - 1));
+    COMPARE(a[0], T(x));
+    return true;
+}
+template <typename V> bool test_pre_decrement(V &&, float) { return false; }
+template <typename V, typename = decltype(--std::declval<V>()[0])>
+bool test_pre_decrement(V &&a, int x)
+{
+    using T = EntryType<V>;
+    COMPARE(--a[0], T(x));
+    COMPARE(a[0], T(x));
+    return true;
+}
+template <typename V> bool test_post_decrement(V &&, float) { return false; }
+template <typename V, typename = decltype(std::declval<V>()[0]--)>
+bool test_post_decrement(V &&a, int x)
+{
+    using T = EntryType<V>;
+    COMPARE(a[0]--, T(x + 1));
+    COMPARE(a[0], T(x));
+    return true;
+}
+
 TEST_TYPES(V, operators, (ALL_VECTORS, SIMD_ARRAY_LIST))
 {
-    using T = typename V::EntryType;
+    using T = EntryType<V>;
     V a = 10;
-    COMPARE(a[0]  += 1, T(11)); COMPARE(a[0], T(11));
-    COMPARE(a[0]  -= 1, T(10)); COMPARE(a[0], T(10));
-    COMPARE(a[0]  *= 2, T(20)); COMPARE(a[0], T(20));
-    COMPARE(a[0]  /= 2, T(10)); COMPARE(a[0], T(10));
-    COMPARE( --a[0]   , T( 9)); COMPARE(a[0], T( 9));
-    COMPARE(   a[0]-- , T( 9)); COMPARE(a[0], T( 8));
-    COMPARE( ++a[0]   , T( 9)); COMPARE(a[0], T( 9));
-    COMPARE(   a[0]++ , T( 9)); COMPARE(a[0], T(10));
-    COMPARE(test_percent_assign<V>(a, 6, 4), std::is_integral<T>::value);
-    COMPARE(test_lshift_assign<V>(a, 1, 8), std::is_integral<T>::value);
-    COMPARE(test_rshift_assign<V>(a, 2, 2), std::is_integral<T>::value);
-    COMPARE(test_bor_assign<V>(a, 9, 11), std::is_integral<T>::value);
-    COMPARE(test_band_assign<V>(a, 13, 9), std::is_integral<T>::value);
-    COMPARE(test_bxor_assign<V>(a, 1, 8), std::is_integral<T>::value);
+    VERIFY(test_plus_assign(a, 1, 11));
+    VERIFY(test_minus_assign(a, 1, 10));
+    VERIFY(test_times_assign(a, 2, 20));
+    VERIFY(test_divide_assign(a, 2, 10));
+    VERIFY(test_pre_decrement(a, 9));
+    VERIFY(test_post_decrement(a, 8));
+    VERIFY(test_pre_increment(a, 9));
+    VERIFY(test_post_increment(a, 10));
+    COMPARE(test_percent_assign(a, 6, 4), std::is_integral<T>::value);
+    COMPARE(test_lshift_assign(a, 1, 8), std::is_integral<T>::value);
+    COMPARE(test_rshift_assign(a, 2, 2), std::is_integral<T>::value);
+    COMPARE(test_bor_assign(a, 9, 11), std::is_integral<T>::value);
+    COMPARE(test_band_assign(a, 13, 9), std::is_integral<T>::value);
+    COMPARE(test_bxor_assign(a, 1, 8), std::is_integral<T>::value);
+
+    // assignment operators should never work on const ref
+    const auto &x = a[0];
+    VERIFY(!test_plus_assign(x, 1, 11));
+    VERIFY(!test_minus_assign(x, 1, 10));
+    VERIFY(!test_times_assign(x, 2, 20));
+    VERIFY(!test_divide_assign(x, 2, 10));
+    VERIFY(!test_pre_decrement(x, 9));
+    VERIFY(!test_post_decrement(x, 8));
+    VERIFY(!test_pre_increment(x, 9));
+    VERIFY(!test_post_increment(x, 10));
+    VERIFY(!test_percent_assign(x, 6, 4));
+    VERIFY(!test_lshift_assign(x, 1, 8));
+    VERIFY(!test_rshift_assign(x, 2, 2));
+    VERIFY(!test_bor_assign(x, 9, 11));
+    VERIFY(!test_band_assign(x, 13, 9));
+    VERIFY(!test_bxor_assign(x, 1, 8));
+
+    // Just to make sure, the assignment operators also should not work on lvalue
+    // references
+    decltype(a[0]) *y;
+    VERIFY(!test_plus_assign(*y, 1, 11));
+    VERIFY(!test_minus_assign(*y, 1, 10));
+    VERIFY(!test_times_assign(*y, 2, 20));
+    VERIFY(!test_divide_assign(*y, 2, 10));
+    VERIFY(!test_pre_decrement(*y, 9));
+    VERIFY(!test_post_decrement(*y, 8));
+    VERIFY(!test_pre_increment(*y, 9));
+    VERIFY(!test_post_increment(*y, 10));
+    VERIFY(!test_percent_assign(*y, 6, 4));
+    VERIFY(!test_lshift_assign(*y, 1, 8));
+    VERIFY(!test_rshift_assign(*y, 2, 2));
+    VERIFY(!test_bor_assign(*y, 9, 11));
+    VERIFY(!test_band_assign(*y, 13, 9));
+    VERIFY(!test_bxor_assign(*y, 1, 8));
 }
