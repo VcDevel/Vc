9cc6a9d4d2133dad008348af99f2138443edc8ba
Matthias Kretz <kretz@kde.org>
2016-03-23 09:50:11 +0100

    Add ElementReference - a generic smart reference
    ElementReference<T> acts as much as possible like an lvalue reference of
    T::value_type. It has:
    * all move and copy ctors and assignment deleted, and
    * an rvalue ref-qualifier on all assignment and in-/decrement operators
    
    to limit the use of the proxy type as much as possible. The intent is to
    either copy to value_type or to modify the referenced element directly,
    i.e. without going through an lvalue.
    
    The noexcept specification is derived from the noexcept-ness of the
    Accessor functions to the original container/vector.
    
    Refs: gh-115
    Signed-off-by: Matthias Kretz <kretz@kde.org>

---
+++ common/elementreference.h
@@ -0,0 +1,165 @@
+/*  This file is part of the Vc library. {{{
+Copyright © 2016 Matthias Kretz <kretz@kde.org>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of contributing organizations nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+}}}*/
+
+#ifndef VC_COMMON_ELEMENTREFERENCE_H_
+#define VC_COMMON_ELEMENTREFERENCE_H_
+
+#include "macros.h"
+
+namespace Vc_VERSIONED_NAMESPACE
+{
+namespace Detail
+{
+template <typename U, typename Accessor = U> class ElementReference
+{
+    using value_type = typename U::value_type;
+    friend U;
+    Vc_INTRINSIC ElementReference(U &o, int i) noexcept : index(i), obj(o) {}
+
+    static constexpr bool get_noexcept =
+        noexcept(Accessor::get(std::declval<U &>(), int()));
+    template <typename T> static constexpr bool set_noexcept()
+    {
+        return noexcept(Accessor::set(std::declval<U &>(), int(), std::declval<T>()));
+    }
+
+public:
+    Vc_INTRINSIC ElementReference(const ElementReference &) = delete;
+
+    Vc_INTRINSIC operator value_type() const noexcept(get_noexcept)
+    {
+        return Accessor::get(obj, index);
+    }
+
+    template <typename T>
+        Vc_INTRINSIC enable_if<
+            std::is_same<void, decltype(Accessor::set(std::declval<U &>(), int(),
+                                                      std::declval<T>()))>::value,
+            ElementReference &>
+        operator=(T &&x) &&
+        noexcept(set_noexcept<T>())
+    {
+        Accessor::set(obj, index, std::forward<T>(x));
+        return *this;
+    }
+
+// TODO: improve with operator.()
+
+#define Vc_OP_(op_)                                                                      \
+    template <typename T, typename R = decltype(std::declval<const value_type &>()       \
+                                                    op_ std::declval<T>())>              \
+        Vc_INTRINSIC enable_if<                                                          \
+            std::is_same<void, decltype(Accessor::set(std::declval<U &>(), int(),        \
+                                                      std::declval<R &&>()))>::value,    \
+            ElementReference &>                                                          \
+        operator op_##=(T &&x) &&                                                        \
+        noexcept(get_noexcept &&                                                         \
+                 set_noexcept<decltype(std::declval<const value_type &>()                \
+                                           op_ std::declval<T>())>())                    \
+    {                                                                                    \
+        const value_type &lhs = Accessor::get(obj, index);                               \
+        Accessor::set(obj, index, lhs op_ std::forward<T>(x));                           \
+        return *this;                                                                    \
+    }
+    Vc_ALL_ARITHMETICS(Vc_OP_);
+    Vc_ALL_SHIFTS(Vc_OP_);
+    Vc_ALL_BINARY(Vc_OP_);
+#undef Vc_OP_
+
+    template <typename... T>
+        Vc_INTRINSIC enable_if<
+            (sizeof...(T) == 0 &&
+             std::is_same<value_type &, decltype(++std::declval<value_type &>())>::value),
+            ElementReference &>
+        operator++(T...) &&
+        noexcept(noexcept(std::declval<value_type &>() =
+                              Accessor::get(std::declval<U &>(), int())) &&
+                 set_noexcept<decltype(++std::declval<value_type &>())>())
+    {
+        value_type x = Accessor::get(obj, index);
+        Accessor::set(obj, index, ++x);
+        return *this;
+    }
+
+    template <typename... T>
+        Vc_INTRINSIC enable_if<
+            (sizeof...(T) == 1 &&
+             std::is_same<value_type &, decltype(++std::declval<value_type &>())>::value),
+            value_type>
+        operator++(T...) &&
+        noexcept(noexcept(std::declval<value_type &>() =
+                              Accessor::get(std::declval<U &>(), int())) &&
+                 set_noexcept<decltype(std::declval<value_type &>()++)>())
+    {
+        const value_type r = Accessor::get(obj, index);
+        value_type x = r;
+        Accessor::set(obj, index, ++x);
+        return r;
+    }
+
+    template <typename... T>
+        Vc_INTRINSIC enable_if<
+            (sizeof...(T) == 0 &&
+             std::is_same<value_type &, decltype(--std::declval<value_type &>())>::value),
+            ElementReference &>
+        operator--(T...) &&
+        noexcept(noexcept(std::declval<value_type &>() =
+                              Accessor::get(std::declval<U &>(), int())) &&
+                 set_noexcept<decltype(--std::declval<value_type &>())>())
+    {
+        value_type x = Accessor::get(obj, index);
+        Accessor::set(obj, index, --x);
+        return *this;
+    }
+
+    template <typename... T>
+        Vc_INTRINSIC enable_if<
+            (sizeof...(T) == 1 &&
+             std::is_same<value_type &, decltype(--std::declval<value_type &>())>::value),
+            value_type>
+        operator--(T...) &&
+        noexcept(noexcept(std::declval<value_type &>() =
+                              Accessor::get(std::declval<U &>(), int())) &&
+                 set_noexcept<decltype(std::declval<value_type &>()--)>())
+    {
+        const value_type r = Accessor::get(obj, index);
+        value_type x = r;
+        Accessor::set(obj, index, --x);
+        return r;
+    }
+
+private:
+    int index;
+    U &obj;
+};
+}  // namespace Detail
+}  // namespace Vc
+
+#endif  // VC_COMMON_ELEMENTREFERENCE_H_
+
+// vim: foldmethod=marker
