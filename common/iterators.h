af821743c7b927d4318f85d111173ff608a2340b
Matthias Kretz <kretz@kde.org>
2016-03-30 11:23:07 +0200

    Port Iterator to ElementReference
    * Set iterator_category to input_iterator_tag
    * Make Iterator DefaultConstructible and CopyAssignable
    * Implement all RandomAccessIterator operations
    * Fix const-correctness (i.e. const mutable iterators still point to
    mutable data)
    * Drop the std::iterator base: It was useless and inflexible
    
    Refs: gh-115
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- common/iterators.h
+++ common/iterators.h
@@ -32,6 +32,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <array>
 #include <iterator>
 #include "where.h"
+#include "elementreference.h"
 #include "macros.h"
 
 namespace Vc_VERSIONED_NAMESPACE
@@ -42,40 +43,109 @@ namespace Common
 template<typename _V, typename Flags> class MemoryVector;
 template<typename _V, typename Flags> class MemoryVectorIterator;
 
-// class Iterator {{{
-template <typename V>
-class Iterator
-    : public std::iterator<std::bidirectional_iterator_tag, typename V::EntryType>
+template <typename V> class Iterator;
+template <typename V, bool> class IteratorBase;
+template <typename V> class IteratorBase<V, true>
+{
+public:
+    using iterator_category = std::input_iterator_tag;
+    using value_type = typename V::value_type;
+    using difference_type = int;
+    using reference = value_type;
+    Vc_ALWAYS_INLINE reference operator*() const { return v()[i()]; }
+    Vc_ALWAYS_INLINE reference operator[](difference_type i2) const { return v()[i2]; }
+
+private:
+    Vc_INTRINSIC V &v() const { return *static_cast<const Iterator<V> *>(this)->v; }
+    Vc_INTRINSIC difference_type i() const
     {
-        V &v;
-        size_t i;
-    public:
-        constexpr Iterator(V &_v, size_t _i) : v(_v), i(_i) {}
-        constexpr Iterator(const Iterator &) = default;
-        constexpr Iterator(Iterator &&) = default;
+        return static_cast<const Iterator<V> *>(this)->i;
+    }
+};
 
-        Vc_ALWAYS_INLINE decltype(v[i]) operator->() { return v[i]; }
-        Vc_ALWAYS_INLINE decltype(v[i]) operator->() const { return v[i]; }
+template <typename V> class IteratorBase<V, false>
+{
+public:
+    using iterator_category = std::input_iterator_tag;
+    using value_type = typename V::value_type;
+    using difference_type = int;
+    using reference = Vc::Detail::ElementReference<V, IteratorBase>;
+    Vc_ALWAYS_INLINE reference operator*() const { return {*v(), i()}; }
+    Vc_ALWAYS_INLINE reference operator[](difference_type i2) const { return {*v(), i2}; }
 
-        Vc_ALWAYS_INLINE decltype(v[i]) operator*() { return v[i]; }
-        Vc_ALWAYS_INLINE decltype(v[i]) operator*() const { return v[i]; }
+private:
+    Vc_INTRINSIC V *v() const { return static_cast<const Iterator<V> *>(this)->v; }
+    Vc_INTRINSIC difference_type i() const
+    {
+        return static_cast<const Iterator<V> *>(this)->i;
+    }
 
-        Vc_ALWAYS_INLINE Iterator &operator++()    { ++i; return *this; }
-        Vc_ALWAYS_INLINE Iterator  operator++(int) { Iterator tmp = *this; ++i; return tmp; }
+    friend reference;
+    static Vc_INTRINSIC value_type get(const V &o, int i)
+    {
+        return o[i];
+    }
+    template <typename T> static Vc_INTRINSIC void set(V &o, int i, T &&v)
+    {
+        o[i] = std::forward<T>(v);
+    }
+};
 
-        Vc_ALWAYS_INLINE Iterator &operator--()    { --i; return *this; }
-        Vc_ALWAYS_INLINE Iterator  operator--(int) { Iterator tmp = *this; --i; return tmp; }
+// class Iterator {{{
+template <typename V> class Iterator : public IteratorBase<V, std::is_const<V>::value>
+{
+    using Base = IteratorBase<V, std::is_const<V>::value>;
+    friend Base;
 
-        // XXX: doesn't check &v == &rhs.v
-        Vc_ALWAYS_INLINE bool operator==(const Iterator<V> &rhs) const { return i == rhs.i; }
-        Vc_ALWAYS_INLINE bool operator!=(const Iterator<V> &rhs) const { return i != rhs.i; }
-        Vc_ALWAYS_INLINE bool operator< (const Iterator<V> &rhs) const { return i <  rhs.i; }
-        Vc_ALWAYS_INLINE bool operator<=(const Iterator<V> &rhs) const { return i <= rhs.i; }
-        Vc_ALWAYS_INLINE bool operator> (const Iterator<V> &rhs) const { return i >  rhs.i; }
-        Vc_ALWAYS_INLINE bool operator>=(const Iterator<V> &rhs) const { return i >= rhs.i; }
-    };/*}}}*/
+public:
+    using typename Base::iterator_category;
+    using typename Base::value_type;
+    using typename Base::difference_type;
+    using pointer = const Iterator *;
+    using typename Base::reference;
 
-    template<typename V> using ConstIterator = Iterator<const V>;
+    constexpr Iterator() = default;
+    constexpr Iterator(V &_v, difference_type _i) : v(&_v), i(_i) {}
+    constexpr Iterator(const Iterator &) = default;
+    constexpr Iterator &operator=(const Iterator &) = default;
+
+    Vc_ALWAYS_INLINE pointer operator->() const { return this; }
+    using Base::operator*;
+
+    Vc_ALWAYS_INLINE Iterator &operator++()    { ++i; return *this; }
+    Vc_ALWAYS_INLINE Iterator  operator++(int) { Iterator tmp = *this; ++i; return tmp; }
+
+    // bidirectional iteration is supported
+    Vc_ALWAYS_INLINE Iterator &operator--()    { --i; return *this; }
+    Vc_ALWAYS_INLINE Iterator  operator--(int) { Iterator tmp = *this; --i; return tmp; }
+
+    // RandomAccessIterator:
+    using Base::operator[];
+    Vc_ALWAYS_INLINE Iterator &operator+=(difference_type d) { i += d; return *this; }
+    Vc_ALWAYS_INLINE Iterator &operator-=(difference_type d) { i -= d; return *this; }
+    Vc_ALWAYS_INLINE Iterator operator+(difference_type d) const { return {*v, i + d}; }
+    Vc_ALWAYS_INLINE Iterator operator-(difference_type d) const { return {*v, i - d}; }
+    Vc_ALWAYS_INLINE difference_type operator-(const Iterator &rhs) const { return i - rhs.i; }
+    friend Vc_ALWAYS_INLINE Iterator operator+(difference_type d, const Iterator &rhs)
+    {
+        return {*rhs.v, rhs.i + d};
+    }
+
+    // InputIterator would not need to test v == rhs.v, but except for `reference` this
+    // class implements a complete RandomAccessIterator
+    Vc_ALWAYS_INLINE bool operator==(const Iterator<V> &rhs) const { return v == rhs.v && i == rhs.i; }
+    Vc_ALWAYS_INLINE bool operator!=(const Iterator<V> &rhs) const { return v == rhs.v && i != rhs.i; }
+    Vc_ALWAYS_INLINE bool operator< (const Iterator<V> &rhs) const { return v == rhs.v && i <  rhs.i; }
+    Vc_ALWAYS_INLINE bool operator<=(const Iterator<V> &rhs) const { return v == rhs.v && i <= rhs.i; }
+    Vc_ALWAYS_INLINE bool operator> (const Iterator<V> &rhs) const { return v == rhs.v && i >  rhs.i; }
+    Vc_ALWAYS_INLINE bool operator>=(const Iterator<V> &rhs) const { return v == rhs.v && i >= rhs.i; }
+
+private:
+    V *v = nullptr;
+    difference_type i = 0;
+};/*}}}*/
+
+template <typename V> using ConstIterator = Iterator<const V>;
 
 #ifdef Vc_IMPL_MIC
     class BitmaskIterator/*{{{*/
@@ -156,34 +226,39 @@ class Iterator
     };/*}}}*/
 #endif
 
-template<typename V> constexpr typename std::enable_if<Traits::is_simd_vector<V>::value, Iterator<V>>::type begin(V &v)
+template <typename T>
+Vc_ALWAYS_INLINE
+    enable_if<Traits::is_simd_vector<T>::value || Traits::is_simd_mask<T>::value,
+              Iterator<typename std::remove_reference<T>::type>>
+    begin(T &&x)
 {
-    return { v, 0 };
+    return {std::forward<T>(x), 0};
 }
 
-template<typename V> constexpr typename std::enable_if<Traits::is_simd_vector<V>::value, Iterator<V>>::type end(V &v)
+template <typename T>
+Vc_ALWAYS_INLINE
+    enable_if<Traits::is_simd_vector<T>::value || Traits::is_simd_mask<T>::value,
+              Iterator<typename std::remove_reference<T>::type>>
+    end(T &&x)
 {
-    return { v, V::Size };
+    using TT = typename std::decay<T>::type;
+    return {std::forward<T>(x), TT::size()};
 }
 
-template<typename V> constexpr typename std::enable_if<Traits::is_simd_mask<V>::value || Traits::is_simd_vector<V>::value, ConstIterator<V>>::type begin(const V &v)
+template <typename T>
+Vc_ALWAYS_INLINE enable_if<
+    Traits::is_simd_mask<T>::value || Traits::is_simd_vector<T>::value, ConstIterator<T>>
+cbegin(const T &v)
 {
-    return { v, 0 };
+    return {v, 0};
 }
 
-template<typename V> constexpr typename std::enable_if<Traits::is_simd_mask<V>::value || Traits::is_simd_vector<V>::value, ConstIterator<V>>::type end(const V &v)
+template <typename T>
+Vc_ALWAYS_INLINE enable_if<
+    Traits::is_simd_mask<T>::value || Traits::is_simd_vector<T>::value, ConstIterator<T>>
+cend(const T &v)
 {
-    return { v, V::Size };
-}
-
-template<typename V> constexpr typename std::enable_if<Traits::is_simd_mask<V>::value || Traits::is_simd_vector<V>::value, ConstIterator<V>>::type cbegin(const V &v)
-{
-    return { v, 0 };
-}
-
-template<typename V> constexpr typename std::enable_if<Traits::is_simd_mask<V>::value || Traits::is_simd_vector<V>::value, ConstIterator<V>>::type cend(const V &v)
-{
-    return { v, V::Size };
+    return {v, T::size()};
 }
 
 template<typename M> Vc_ALWAYS_INLINE BitmaskIterator begin(const WhereImpl::WhereMask<M> &w)
@@ -224,6 +299,8 @@ template<typename V, typename Flags, typename FlagsX> Vc_ALWAYS_INLINE MemoryVec
 
 using Common::begin;
 using Common::end;
+using Common::cbegin;
+using Common::cend;
 using Common::makeIterator;
 }  // namespace Vc
 
