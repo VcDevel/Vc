5ea612d1c2474d7e03108444ba23ec095a7d62dc
Matthias Kretz <kretz@kde.org>
2016-04-11 10:58:06 +0200

    Drop 'all rights reserved'
    The phrase is obsolete since 2000 according to Wikipedia.
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- cmake/FindMIC.cmake
+++ cmake/FindMIC.cmake
@@ -1,6 +1,5 @@
 #=============================================================================
 # Copyright © 2010-2015 Matthias Kretz <kretz@kde.org>
-# All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
--- mic/vector.h
+++ mic/vector.h
@@ -1,6 +1,5 @@
 /*  This file is part of the Vc library. {{{
 Copyright © 2009-2015 Matthias Kretz <kretz@kde.org>
-All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
