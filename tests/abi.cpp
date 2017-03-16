73c33d295b00fe5e20c461a7c1fb1db0584b12ad
Matthias Kretz <kretz@kde.org>
2017-03-16 13:54:22 +0100

    Fix ABI tests to not require Vc_IMPL anymore
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- tests/abi.cpp
+++ tests/abi.cpp
@@ -1,6 +1,6 @@
 #include <Vc/datapar>
 
-using float_v = Vc::datapar<float, Vc::datapar_abi::native<float>>;
+using float_v = Vc::datapar<float, Vc::datapar_abi::Vc_ABI>;
 using float_m = typename float_v::mask_type;
 
 float_v test(float_v a, float_v b)
