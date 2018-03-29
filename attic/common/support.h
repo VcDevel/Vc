e22553c49772568cdef2f38217dcedf76434dfa3
Matthias Kretz <kretz@kde.org>
2018-03-29 17:17:00 +0200

    Remove Vc 1.3 sources
    This is just confusing on greps
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- attic/common/support.h
+++
@@ -1,7 +0,0 @@
-#ifndef VC_DEPRECATED_COMMON_SUPPORT_H_
-#define VC_DEPRECATED_COMMON_SUPPORT_H_
-#ifdef __GNUC__
-#warning "the <Vc/common/support.h> header is deprecated. Use <Vc/support.h> instead."
-#endif
-#include <Vc/support.h>
-#endif // VC_DEPRECATED_COMMON_SUPPORT_H_
