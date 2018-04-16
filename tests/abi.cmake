25505f4030dcd4d4386e728f8b527874625152aa
Matthias Kretz <kretz@kde.org>
2018-04-16 15:15:23 +0200

    Support new ABI check with ICC/MIC
    Refs: gh-191
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- tests/abi.cmake
+++ tests/abi.cmake
@@ -6,7 +6,8 @@ macro(extract_asm marker)
    execute_process(
       COMMAND grep -A100 ${marker}_start ${ASM}
       COMMAND grep -B100 ${marker}_end
-      COMMAND grep -v -e ${marker}_ -e "#"
+      COMMAND sed -e "s/ *#.*$//"
+      COMMAND grep -v -e ${marker}_ -e "^\\." -e "^ *$"
       OUTPUT_VARIABLE asm)
    string(STRIP "${asm}" asm)
    string(REPLACE "(%rip)" "" asm "${asm}")
@@ -17,11 +18,11 @@ endmacro()
 extract_asm(vector_abi)
 
 if("${IMPL}" STREQUAL SSE)
-   set(reference "v?movaps a_v, %xmm0.*call .* v?movaps %xmm0, b_v")
+   set(reference "v?movaps a_v, %xmm0.*call.* v?movaps %xmm0, b_v")
 elseif("${IMPL}" STREQUAL AVX OR "${IMPL}" STREQUAL AVX2)
-   set(reference "vmovaps a_v, %ymm0.*call .* vmovaps %ymm0, b_v")
+   set(reference "vmovaps a_v, %ymm0.*call.* vmovaps %ymm0, b_v")
 elseif("${IMPL}" STREQUAL MIC)
-   set(reference "^vaddps %zmm(0,%zmm1|1,%zmm0),%zmm0 retq")
+   set(reference "vmovaps a_v, %zmm0.*call.* vmovaps %zmm0, b_v")
 else()
    message(FATAL_ERROR "Unknown IMPL '${IMPL}'")
 endif()
@@ -36,11 +37,12 @@ endif()
 # test Mask<T> ABI
 #######################################################################
 if("${IMPL}" STREQUAL MIC)
-   set(reference "%di,.*%si,") # needs to read from %di and %si
+   set(reference "movzwl a_m, %edi.*call.*movw %ax, b_m")
+else()
+   string(REPLACE "_v" "_m" reference "${reference}")
 endif()
 
 extract_asm(mask_abi)
-string(REPLACE "_v" "_m" reference "${reference}")
 if("${asm}" MATCHES "${reference}")
    message("PASS: Mask<T> ABI")
 else()
