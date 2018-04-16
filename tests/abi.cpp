71a817121cb9455e6149d5a4053d1225a71a1496
Matthias Kretz <kretz@kde.org>
2018-04-16 15:14:48 +0200

    Clang: make the ABI test work
    Clang requires a real asm label (for its integrated assembler) and -O1
    to get rid of a stack copy of the argument and return value.
    
    Refs: gh-191
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- tests/abi.cpp
+++ tests/abi.cpp
@@ -8,14 +8,14 @@ Vc::float_m a_m, b_m;
 
 void my_test1()
 {
-    asm volatile("vector_abi_start");
+    asm volatile("vector_abi_start:");
     b_v = test_vector_abi(a_v);
-    asm volatile("vector_abi_end");
+    asm volatile("vector_abi_end:");
 }
 
 void my_test2()
 {
-    asm volatile("mask_abi_start");
+    asm volatile("mask_abi_start:");
     b_m = test_mask_abi(a_m);
-    asm volatile("mask_abi_end");
+    asm volatile("mask_abi_end:");
 }
