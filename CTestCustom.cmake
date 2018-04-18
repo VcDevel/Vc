ac2b95019c387a3a37bba743edc4ec3b2835922d
Matthias Kretz <kretz@kde.org>
2018-04-18 14:24:26 +0200

    CTest: drop old warning suppression
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- CTestCustom.cmake
+++ CTestCustom.cmake
@@ -6,7 +6,6 @@ set(CTEST_CUSTOM_WARNING_EXCEPTION ${CTEST_CUSTOM_WARNING_EXCEPTION}
    "warning is a GCC extension"
    "^-- "  # Ignore output from cmake
    "AVX disabled per default because of old/broken compiler" # This warning is meant for users not the dashboard
-   "warning: the mangled name of .*typename Vc::{anonymous}::Decltype.* will change in a future version of GCC"
    "WARNING non-zero return value in ctest from: make" # Ignore output from ctest
    "ipo: warning #11010:" # Ignore warning about incompatible libraries with ICC -m32 on 64-bit system
    "include/qt4" # -Wuninitialized in QWeakPointer(X *ptr)
