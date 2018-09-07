c78174fb6c40db4e30d7e43914c1a20ddd78ff32
Matthias Kretz <kretz@kde.org>
2018-09-07 09:09:55 +0200

    Fix relative include to use quotes
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- tests/arithmetics.cpp
+++ tests/arithmetics.cpp
@@ -29,8 +29,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <iostream>
 #include <limits>
 #include <Vc/limits>
-#include <../Vc/common/const.h>
-#include <../Vc/common/macros.h>
+#include "../Vc/common/const.h"
+#include "../Vc/common/macros.h"
 #include <random>
 
 using namespace Vc;
