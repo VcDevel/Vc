b725a903b54ae64364ec8bfee9d37e5fd033ae38
Matthias Kretz <kretz@kde.org>
2018-12-10 14:24:10 +0100

    Fix apidox script to use new paths
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- makeApidoxLoop.sh
+++ makeApidoxLoop.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+cd "`dirname "$0"`"
 test -f .makeApidox.stamp || touch .makeApidox.stamp
 while true; do
-  ls -l -t include/Vc/* doc/dox.h doc/examples.h common/*|head -n1 > .makeApidox.stamp.new
+  ls -l -t Vc/* doc/dox.h doc/examples.h Vc/common/*|head -n1 > .makeApidox.stamp.new
   if ! diff -q .makeApidox.stamp.new .makeApidox.stamp; then
     ./makeApidox.sh
     mv -f .makeApidox.stamp.new .makeApidox.stamp
