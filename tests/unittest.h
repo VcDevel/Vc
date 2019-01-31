c6310fec2633e910b7942b22872df4e3d7ea649e
Matthias Kretz <kretz@kde.org>
2019-01-31 13:11:11 +0100

    Make sure we include our own simd headers
    and not the one installed with the standard library
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- tests/unittest.h
+++ tests/unittest.h
@@ -25,7 +25,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 }}}*/
 
-#include <experimental/simd>
+#include "../experimental/simd"
 #include <vir/test.h>
 #include <iostream>
 #include <iomanip>
