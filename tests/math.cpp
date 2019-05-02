5e96dfbf003ca5a64f34508ae89344f1ce29f50c
Matthias Kretz <kretz@kde.org>
2019-05-02 21:32:53 +0200

    Test all rounding modes for the math tests
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- tests/math.cpp
+++ tests/math.cpp
@@ -198,7 +198,7 @@ TEST_TYPES(V, frexp, real_test_types)  //{{{1
          -7,  -8,   -9,  -10, -11, -12, -13, -14,   -15,  -16, -17, -18, -19,
          -20, -21,  -22, -23, -24, -25, -26, -27,   -28,  -29, -32, -31,
          limits::max(), -limits::max(),
-         limits::max() * 0.123, -limits::max() * 0.123,
+         limits::max() * 0.123f, -limits::max() * 0.123f,
          limits::denorm_min(), -limits::denorm_min(),
          limits::min() / 2, -limits::min() / 2},
         [](const V input) {
