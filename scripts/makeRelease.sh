da38e7a02ec2f0a331d450000dc2169c75afb4d9
Matthias Kretz <kretz@kde.org>
2019-05-07 08:55:01 +0200

    Fix release tarball creation
    1. include virtest
    2. ensure the tag for the version doesn't exist
    3. don't pack dotfiles
    
    Signed-off-by: Matthias Kretz <kretz@kde.org>

--- scripts/makeRelease.sh
+++ scripts/makeRelease.sh
@@ -4,6 +4,12 @@ cd "`dirname "$0"`/.."
 
 # Read version number
 version=`grep 'SIMD_VERSION_STRING "[0-9.]\+"' experimental/bits/simd_detail.h|cut -d'"' -f2`
+
+if git tag|grep -q "^simd-$version$"; then
+  echo "The tag 'simd-$version' already exists. Aborting." >&2
+  exit 1
+fi
+
 echo "new release version: $version"
 echo -n "correct? [y/n]"
 read -n 1 ok
@@ -14,5 +20,9 @@ fi
 
 git tag -m "std::experimental::simd $version release" -s "simd-$version" || exit
 
+# Make sure git-archive-all (needed for archiving with virtest) is available
+which git-archive-all >/dev/null 2>&1 || pip install --user git-archive-all || exit
+
 # Create tarball
-git archive --format=tar --prefix="simd-$version/" "$version" | gzip > ../"simd-$version.tar.gz"
+git-archive-all -v --prefix="simd-$version/" "../simd-$version.tar.gz" && \
+  echo "Release tarball at ../simd-$version.tar.gz"
