53a102fe1906ca59fdd307c1100e72db16a5eef8
Axel Naumann <Axel.Naumann@cern.ch>
2022-05-20 15:36:24 +0200

    release: version 1.4.3
    * change version strings/numbers to 1.4.3
    * disable HTML_TIMESTAMP for doxygen
    * don't build tests with make all
    * Add documentation link to Vc-1.4.3
    
    Signed-off-by: Axel Naumann <Axel.Naumann@cern.ch>

--- Vc/version.h
+++ Vc/version.h
@@ -37,13 +37,13 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * \ingroup Utilities
  * Contains the version string of the %Vc headers. Same as Vc::versionString().
  */
-#define Vc_VERSION_STRING "1.4.2-dev"
+#define Vc_VERSION_STRING "1.4.3"
 
 /**
  * \ingroup Utilities
  * Contains the encoded version number of the %Vc headers. Same as Vc::versionNumber().
  */
-#define Vc_VERSION_NUMBER 0x010405
+#define Vc_VERSION_NUMBER 0x010406
 
 /**
  * \ingroup Utilities
